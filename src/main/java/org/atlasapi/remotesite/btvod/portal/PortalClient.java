fc2761f2ce0d6b575c658c96de0fac4241712384
Tom McAdam <tom@metabroadcast.com>
2015-01-16 10:19:29 +0000

    Treat a 404 as no content
    The BT API cannot return an emptyset for a valid,
    but empty, group. Therefore we will catch that case
    and interpret any 404 as an empty group.

--- src/main/java/org/atlasapi/remotesite/btvod/portal/PortalClient.java
+++ src/main/java/org/atlasapi/remotesite/btvod/portal/PortalClient.java
@@ -2,8 +2,21 @@ package org.atlasapi.remotesite.btvod.portal;
 
 import java.util.Set;
 
+import com.google.common.base.Optional;
+
 
 public interface PortalClient {
 
-    Set<String> getProductIdsForGroup(String groupId);
+    /**
+     * Provide the members of a given groupId. Will return 
+     * {@link Optional.absent()} in the case of the group not 
+     * being present. The group will not be present in two cases:
+     * 
+     *  1) If the group is not set up
+     *  2) If there are no entries in a valid group
+     * 
+     * @param groupId
+     * @return
+     */
+    Optional<Set<String>> getProductIdsForGroup(String groupId);
 }

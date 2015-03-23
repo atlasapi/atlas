110abed1dfed7aa3c413b02bbd865a432efe7595
Wojtek Wajerowicz <wojtek@metabroadcast.com>
2015-03-23 17:36:28 +0000

    Add terms of use to content.

--- src/main/java/org/atlasapi/output/simple/ContentModelSimplifier.java
+++ src/main/java/org/atlasapi/output/simple/ContentModelSimplifier.java
@@ -160,6 +160,7 @@ public abstract class ContentModelSimplifier<F extends Content, T extends Descri
                 return eventRefSimplifier.simplify(input, annotations, config); 
             }
         }));
+        simpleDescription.setTermsOfUse(content.getTermsOfUse());
         
     }
     

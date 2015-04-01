af1af934a16eec087c37efadb96bbe3262cbf63c
Tom McAdam <tom@metabroadcast.com>
2015-04-01 16:25:54 +0100

    Add missing import

--- src/main/java/org/atlasapi/system/ScrubbablesProducerContentPurgeController.java
+++ src/main/java/org/atlasapi/system/ScrubbablesProducerContentPurgeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.google.common.collect.ImmutableSet;
+import com.metabroadcast.common.http.HttpStatusCode;
 
 
 public class ScrubbablesProducerContentPurgeController {

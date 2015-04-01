3eb96f7ad544cac238b66ff21824bc51745509f8
Tom McAdam <tom@metabroadcast.com>
2015-04-01 16:18:43 +0100

    Add endpoint to purge SCRUBBABLES_PRODUCER content

---
+++ src/main/java/org/atlasapi/system/ScrubbablesProducerContentPurgeController.java
@@ -0,0 +1,28 @@
+package org.atlasapi.system;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.persistence.content.ContentPurger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.common.collect.ImmutableSet;
+
+
+public class ScrubbablesProducerContentPurgeController {
+
+ private final ContentPurger contentPurger;
+    
+    public ScrubbablesProducerContentPurgeController(ContentPurger contentPurger) {
+        this.contentPurger = checkNotNull(contentPurger);
+    }
+    
+    @RequestMapping(value = "/system/content/purge/scrubbables-producer", method = RequestMethod.POST)
+    public void purge(HttpServletResponse response) {
+        contentPurger.purge(Publisher.SCRUBBABLES_PRODUCER, ImmutableSet.<Publisher>of());
+        response.setStatus(HttpStatusCode.OK.code());
+    }
+}

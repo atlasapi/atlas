cf7f298c3efbb13603cded6ed68fbff8d9b4aac6
Will Berg <will@metabroadcast.com>
2015-04-01 16:54:38 +0100

    Use Google charset to avoid throwing exception

--- src/main/java/org/atlasapi/remotesite/knowledgemotion/KnowledgeMotionDataRowContentExtractor.java
+++ src/main/java/org/atlasapi/remotesite/knowledgemotion/KnowledgeMotionDataRowContentExtractor.java
@@ -29,6 +29,7 @@ import org.joda.time.format.PeriodFormatterBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
@@ -46,17 +47,11 @@ public class KnowledgeMotionDataRowContentExtractor implements ContentExtractor<
     private static final Function<String, String> PRICE_CATEGORY_TO_GENRE = new Function<String, String>() {
         @Override
         public String apply(String priceCategory) {
-            try {
-                return String.format(
-                        "%s%s",
-                        KNOWLEDGEMOTION_GENRE_PREFIX,
-                        URLEncoder.encode(priceCategory, "UTF-8")
-                );
-            } catch (UnsupportedEncodingException e) {
-                //this should never happen
-                log.error("Error encoding price category", e);
-                return null;
-            }
+            return String.format(
+                    "%s%s",
+                    KNOWLEDGEMOTION_GENRE_PREFIX,
+                    Charsets.UTF_8
+            );
         }
     };
 

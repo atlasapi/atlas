f1d0ebead04acf1f272e02789d8028c7697b117e
Tom McAdam <tom@metabroadcast.com>
2015-05-21 22:13:36 +0100

    Deactive old content in BT VoD catalogue

---
+++ src/main/java/org/atlasapi/remotesite/btvod/MultiplexingVodContentListener.java
@@ -0,0 +1,24 @@
+package org.atlasapi.remotesite.btvod;
+
+import java.util.List;
+
+import org.atlasapi.media.entity.Content;
+import org.atlasapi.remotesite.btvod.model.BtVodEntry;
+
+import com.google.common.collect.ImmutableList;
+
+public class MultiplexingVodContentListener implements BtVodContentListener {
+
+    private final ImmutableList<BtVodContentListener> delegates;
+
+    public MultiplexingVodContentListener(List<BtVodContentListener> delegates) {
+        this.delegates = ImmutableList.copyOf(delegates);
+    }
+
+    @Override
+    public void onContent(Content content, BtVodEntry vodData) {
+        for (BtVodContentListener delegate : delegates) {
+            delegate.onContent(content, vodData);
+        }
+    }
+}
---
+++ src/test/java/org/atlasapi/remotesite/util/OldContentDeactivatorTest.java
@@ -0,0 +1,116 @@
+package org.atlasapi.remotesite.util;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Set;
+
+import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.Identified;
+import org.atlasapi.media.entity.Item;
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.media.entity.testing.ComplexItemTestDataBuilder;
+import org.atlasapi.media.entity.testing.ItemTestDataBuilder;
+import org.atlasapi.persistence.content.ContentResolver;
+import org.atlasapi.persistence.content.ContentWriter;
+import org.atlasapi.persistence.content.ResolvedContent;
+import org.atlasapi.persistence.content.listing.ContentLister;
+import org.atlasapi.persistence.content.listing.ContentListingCriteria;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+
+
+@RunWith( MockitoJUnitRunner.class )
+public class OldContentDeactivatorTest {
+
+    private static final Publisher PUBLISHER = Publisher.METABROADCAST;
+    private final ContentListingCriteria expectedCriteria 
+                    = new ContentListingCriteria.Builder().forPublisher(PUBLISHER).build();
+    
+    private final Content item1 = ComplexItemTestDataBuilder
+                                    .complexItem().withUri("http://example.org/1").build();
+    private final Content item2 = ComplexItemTestDataBuilder
+                                    .complexItem().withUri("http://example.org/2").build();
+    private final Content item3 = ComplexItemTestDataBuilder
+                                    .complexItem().withUri("http://example.org/3").build();
+    private final Content item4 = ComplexItemTestDataBuilder
+                                    .complexItem().withUri("http://example.org/4").build();
+    
+    private final Set<Content> allContent = ImmutableSet.of(item1, item2, item3, item4);
+    
+    @Mock
+    private ContentLister contentLister;
+    @Mock
+    private ContentWriter contentWriter;
+    @Mock
+    private ContentResolver contentResolver;
+    @InjectMocks
+    private OldContentDeactivator oldContentDeactivator;
+    
+    @Test
+    public void testRemovesContentIfNoThreshold() {
+        
+        when(contentLister.listContent(expectedCriteria))
+            .thenReturn(allContent.iterator());
+        setUpResolverForContent(item1);
+        
+        oldContentDeactivator.deactivateOldContent(PUBLISHER, 
+                ImmutableSet.of(item2.getCanonicalUri(), item3.getCanonicalUri(), item4.getCanonicalUri()), null);
+        
+        ArgumentCaptor<Item> itemCaptor = ArgumentCaptor.forClass(Item.class);
+        
+        verify(contentWriter).createOrUpdate(itemCaptor.capture());
+        
+        assertFalse(itemCaptor.getValue().isActivelyPublished());
+        assertThat(itemCaptor.getValue().getCanonicalUri(), is(item1.getCanonicalUri()));
+    }
+    
+    @Test
+    public void testDoesntRemoveContentIfThresholdExceeded() {
+        when(contentLister.listContent(expectedCriteria))
+            .thenReturn(allContent.iterator());
+        
+        oldContentDeactivator.deactivateOldContent(PUBLISHER, 
+                ImmutableSet.of(item2.getCanonicalUri()), 50);
+        
+        verifyNoMoreInteractions(contentWriter);
+    }
+    
+    @Test
+    public void testRemovesContentIfThresholdSpecifiedAndNotExceeded() {
+        when(contentLister.listContent(expectedCriteria))
+            .thenReturn(allContent.iterator());
+        
+        setUpResolverForContent(item1);
+        
+        oldContentDeactivator.deactivateOldContent(PUBLISHER, 
+                ImmutableSet.of(item2.getCanonicalUri(), item3.getCanonicalUri(), item4.getCanonicalUri()), 70);
+        
+        ArgumentCaptor<Item> itemCaptor = ArgumentCaptor.forClass(Item.class);
+        
+        verify(contentWriter).createOrUpdate(itemCaptor.capture());
+        
+        assertFalse(itemCaptor.getValue().isActivelyPublished());
+        assertThat(itemCaptor.getValue().getCanonicalUri(), is(item1.getCanonicalUri()));
+    }
+    
+    private void setUpResolverForContent(Content c) {
+        when(contentResolver.findByCanonicalUris(ImmutableSet.of(item1.getCanonicalUri())))
+            .thenReturn( new ResolvedContent
+                               .ResolvedContentBuilder()
+                               .put(c.getCanonicalUri(), c).build());
+    }
+}

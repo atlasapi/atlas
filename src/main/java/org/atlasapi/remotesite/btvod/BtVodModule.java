007dbe4a97391323f7bdbb4d7f9b0502b278e69c
Wojtek Wajerowicz <wojtek@metabroadcast.com>
2015-07-08 17:43:21 +0100

    Add q param to BT VoD ingest

--- src/main/java/org/atlasapi/remotesite/btvod/BtVodModule.java
+++ src/main/java/org/atlasapi/remotesite/btvod/BtVodModule.java
@@ -67,7 +67,9 @@ public class BtVodModule {
     private String btPortalContentGroupsBaseUri;
     @Value("${bt.vod.mpx.feed.baseUrl}")
     private String btVodMpxFeedBaseUrl;
-    
+    @Value("${bt.vod.mpx.feed.params.q")
+    private String btVodMpxFeedQParam;
+
     @Bean
     public BtVodUpdater btVodUpdater() {
         return new BtVodUpdater(contentResolver, 
@@ -96,7 +98,7 @@ public class BtVodModule {
     public HttpBtMpxVodClient mpxVodClient() {
         return new HttpBtMpxVodClient(
                 new SimpleHttpClientBuilder().withUserAgent(HttpClients.ATLAS_USER_AGENT).build(),
-                new HttpBtMpxFeedRequestProvider(btVodMpxFeedBaseUrl)
+                new HttpBtMpxFeedRequestProvider(btVodMpxFeedBaseUrl, btVodMpxFeedQParam)
         );
     }
     

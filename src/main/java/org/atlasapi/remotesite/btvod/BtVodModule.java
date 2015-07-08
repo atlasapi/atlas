16a6598de38ae2bfc29d53e803d09c7ff0efa335
Wojtek Wajerowicz <wojtek@metabroadcast.com>
2015-07-08 17:43:21 +0100

    Add q param to BT VoD ingest

--- src/main/java/org/atlasapi/remotesite/btvod/BtVodModule.java
+++ src/main/java/org/atlasapi/remotesite/btvod/BtVodModule.java
@@ -90,6 +90,9 @@ public class BtVodModule {
     private String btPortalContentGroupsBaseUri;
     @Value("${bt.vod.mpx.feed.baseUrl}")
     private String btVodMpxFeedBaseUrl;
+
+    @Value("${bt.vod.mpx.feed.params.q")
+    private String btVodMpxFeedQParam;
    
     
     @Bean
@@ -182,7 +185,7 @@ public class BtVodModule {
     public HttpBtMpxVodClient mpxVodClient() {
         return new HttpBtMpxVodClient(
                 new SimpleHttpClientBuilder().withUserAgent(HttpClients.ATLAS_USER_AGENT).build(),
-                new HttpBtMpxFeedRequestProvider(btVodMpxFeedBaseUrl)
+                new HttpBtMpxFeedRequestProvider(btVodMpxFeedBaseUrl, btVodMpxFeedQParam)
         );
     }
     
--- src/main/java/org/atlasapi/remotesite/btvod/HttpBtMpxFeedRequestProvider.java
+++ src/main/java/org/atlasapi/remotesite/btvod/HttpBtMpxFeedRequestProvider.java
@@ -1,23 +1,52 @@
 package org.atlasapi.remotesite.btvod;
 
+import com.google.api.client.repackaged.com.google.common.base.Strings;
+import com.google.api.client.repackaged.com.google.common.base.Throwables;
+import com.google.common.base.Optional;
 import com.metabroadcast.common.http.SimpleHttpRequest;
 import org.atlasapi.remotesite.btvod.model.BtVodResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class HttpBtMpxFeedRequestProvider {
 
+    private static final Logger log = LoggerFactory.getLogger(HttpBtMpxFeedRequestProvider.class);
     private final String urlBase;
+    private final Optional<String> query;
 
-    public HttpBtMpxFeedRequestProvider(String urlBase) {
+    public HttpBtMpxFeedRequestProvider(String urlBase, String query) {
         this.urlBase = checkNotNull(urlBase);
+        this.query = Optional.fromNullable(Strings.emptyToNull(query));
     }
 
 
     public SimpleHttpRequest<BtVodResponse> buildRequest(String endpoint, Integer startIndex) {
+        String url = String.format(
+                "%s%s?startIndex=%s%s",
+                urlBase,
+                endpoint,
+                startIndex,
+                additionalQueryParams()
+        );
+        log.debug("Calling BT VoD MPX feed url {}", url);
         return new SimpleHttpRequest<>(
-                String.format("%s%s?startIndex=%s", urlBase, endpoint, startIndex),
+                url,
                 new BtVodResponseTransformer()
+        );
+    }
+
+    private String additionalQueryParams() {
+        if (!query.isPresent()) {
+            return "";
+        }
+        return String.format(
+                "&q=%s",
+                query.get()
         );
     }
 }

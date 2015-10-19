b18f323f81ebacc908a384f70adc31739f7ef4b2
Dias Saparov <dias@metabroadcast.com>
2015-10-19 17:16:57 +0100

    test

--- src/test/java/org/atlasapi/remotesite/wikipedia/TvExtractionTests.java
+++ src/test/java/org/atlasapi/remotesite/wikipedia/TvExtractionTests.java
@@ -33,7 +33,6 @@ public class TvExtractionTests {
                 @Override
                 public TvBrandHierarchy extract(ScrapedFlatHierarchy source) {
                     Assert.assertEquals(145, source.getEpisodes().size()); // TODO should the pilot count?
-                    System.out.println(source.getBrandInfo().image);
                     Assert.assertEquals("Buffy the Vampire Slayer", source.getBrandInfo().title);
                     return super.extract(source);
                 }

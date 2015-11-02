8992034f3eed66dac03658e25593bfc8a9346978
Jamie <jamie@metabroadcast.com>
2015-11-02 17:13:39 +0000

    Everyone loves a loading cache

--- src/main/java/org/atlasapi/remotesite/btvod/HttpBtMpxVodClient.java
+++ src/main/java/org/atlasapi/remotesite/btvod/HttpBtMpxVodClient.java
@@ -1,7 +1,11 @@
 package org.atlasapi.remotesite.btvod;
 
+import com.google.api.client.repackaged.com.google.common.base.Throwables;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
@@ -12,6 +16,8 @@ import org.atlasapi.remotesite.btvod.model.BtVodResponse;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -22,6 +28,16 @@ public class HttpBtMpxVodClient implements BtMpxVodClient {
     private final SimpleHttpClient httpClient;
     private final HttpBtMpxFeedRequestProvider requestProvider;
     private final int pageSize;
+    private final LoadingCache<String, Optional<BtVodEntry>> guidLookupCache = CacheBuilder.newBuilder()
+            .expireAfterAccess(10, TimeUnit.SECONDS)
+            .maximumSize(2000)
+            .build(new CacheLoader<String, Optional<BtVodEntry>>() {
+                @Override
+                public Optional<BtVodEntry> load(String guid) throws Exception {
+                    BtVodResponse response = httpClient.get(requestProvider.buildRequestForSingleAsset(guid));
+                    return Optional.fromNullable(Iterables.getFirst(response.getEntries(), null));
+                }
+            });
 
     public HttpBtMpxVodClient(SimpleHttpClient httpClient, HttpBtMpxFeedRequestProvider requestProvider) {
         this(httpClient, requestProvider, DEFAULT_PAGE_SIZE);
@@ -72,10 +88,9 @@ public class HttpBtMpxVodClient implements BtMpxVodClient {
     @Override
     public Optional<BtVodEntry> getItem(String guid) {
         try {
-            BtVodResponse response = httpClient.get(requestProvider.buildRequestForSingleAsset(guid));
-            return Optional.fromNullable(Iterables.getFirst(response.getEntries(), null));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            return guidLookupCache.get(guid);
+        } catch (ExecutionException e) {
+            throw Throwables.propagate(e);
         }
     }
 }

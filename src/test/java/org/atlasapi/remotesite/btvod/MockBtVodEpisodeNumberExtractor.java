6e47db39df5d8e1958eb80c48bdeaa00d98f175c
Jamie <jamie@metabroadcast.com>
2015-11-02 16:26:26 +0000

    Have mock episode number extractor return number without checking MPX for collection status

--- src/test/java/org/atlasapi/remotesite/btvod/MockBtVodEpisodeNumberExtractor.java
+++ src/test/java/org/atlasapi/remotesite/btvod/MockBtVodEpisodeNumberExtractor.java
@@ -1,11 +1,19 @@
 package org.atlasapi.remotesite.btvod;
 
+import com.google.common.collect.Iterables;
+import com.google.common.primitives.Ints;
 import org.atlasapi.remotesite.btvod.model.BtVodEntry;
 
 public class MockBtVodEpisodeNumberExtractor implements BtVodEpisodeNumberExtractor {
 
     @Override
     public Integer extractEpisodeNumber(BtVodEntry row) {
-        return null;
+        String episodeNumber = Iterables.getOnlyElement(
+                row.getProductScopes()
+        ).getProductMetadata().getEpisodeNumber();
+        if (episodeNumber == null) {
+            return null;
+        }
+        return Ints.tryParse(episodeNumber);
     }
 }

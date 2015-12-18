43f6751f957c1ed91c08712652cbed6cf09c9044
Martins Irbe <martins.git@irbe.co.uk>
2015-12-18 16:16:37 +0000

    Added ancestor title, contributions and images mixins to OffScheduleContentIngestTask and PidUpdateController.

--- src/main/java/org/atlasapi/remotesite/bbc/nitro/OffScheduleContentIngestTask.java
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/OffScheduleContentIngestTask.java
@@ -58,7 +58,8 @@ public class OffScheduleContentIngestTask extends ScheduledTask {
     protected void runTask() {
         ProgrammesQuery query = ProgrammesQuery
                 .builder()
-                .withMixins(ProgrammesMixin.TITLES, ProgrammesMixin.PEOPLE, ProgrammesMixin.GENRE_GROUPINGS)
+                .withMixins(ProgrammesMixin.ANCESTOR_TITLES, ProgrammesMixin.CONTRIBUTIONS,
+                        ProgrammesMixin.GENRE_GROUPINGS, ProgrammesMixin.IMAGES)
                 .withAvailability(AvailabilityOption.AVAILABLE)
                 .withPageSize(pageSize)
                 .withAvailabilityEntityType(AvailabilityEntityTypeOption.EPISODE)

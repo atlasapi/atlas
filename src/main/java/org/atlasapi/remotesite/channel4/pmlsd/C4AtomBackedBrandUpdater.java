b1659cb6f79252d4e9c43fdad0c4d7367faf3793
Dias Saparov <dias@metabroadcast.com>
2016-01-29 10:51:06 +0000

    check aliases for equivalence before merging them

--- src/main/java/org/atlasapi/remotesite/channel4/pmlsd/C4AtomBackedBrandUpdater.java
+++ src/main/java/org/atlasapi/remotesite/channel4/pmlsd/C4AtomBackedBrandUpdater.java
@@ -409,7 +409,7 @@ public class C4AtomBackedBrandUpdater implements C4BrandUpdater {
             copyLastUpdated(fetched, existing);
         }
 
-        if (fetched.getAliases() != null) {
+        if (!existing.getAliases().equals(fetched.getAliases())) {
             existing.setAliases(fetched.getAliases());
             copyLastUpdated(fetched, existing);
         }

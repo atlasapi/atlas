b085ce173bb73e10cda5ed8c3f8e2bcbe48e2ca9
Thomas Maroulis <thomas@metabroadcast.com>
2016-03-18 11:50:08 +0000

    Add async option for /content POST API
    - Add async parameter on the controller to switch between writing
      content immediately and sending a message to a queue for async
      writing
    - Create message consumer and worker for processing content write
      messages
    - The new worker is added to the QueryExecutorModule to keep all
      write responsibilities in one place and to avoid the more
      complicated dependency graph that a separate WorkerModule would
      have generated
    - Remove unused AtlasProcessingWebApplicationContext and processing.xml
    - Add QueryExecutorModule to application context for when running as
      processing. When running as an app server it is inherited via
      QueryWebModule instead

---
+++ src/main/java/org/atlasapi/query/worker/ContentWriteMessageConfiguration.java
@@ -0,0 +1,20 @@
+package org.atlasapi.query.worker;
+
+import com.metabroadcast.common.time.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public abstract class ContentWriteMessageConfiguration {
+
+    @JsonCreator
+    public ContentWriteMessageConfiguration(
+            @JsonProperty("messageId") String messageId,
+            @JsonProperty("timestamp") Timestamp timestamp,
+            @JsonProperty("contentBytes") byte[] contentBytes,
+            @JsonProperty("contentId") long contentId,
+            @JsonProperty("shouldMerge") boolean shouldMerge
+    ) {
+
+    }
+}

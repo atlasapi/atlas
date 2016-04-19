c373269fa9d1978884ee42516e843fa8b7b250ef
Dias Saparov <dias@metabroadcast.com>
2016-04-19 18:08:09 +0100

    added exclusion of content in blacklist from being equivalated

--- src/main/java/org/atlasapi/equiv/generators/EquivalenceGenerators.java
+++ src/main/java/org/atlasapi/equiv/generators/EquivalenceGenerators.java
@@ -1,30 +1,41 @@
 package org.atlasapi.equiv.generators;
 
 import java.util.List;
+import java.util.Set;
 
 import org.atlasapi.equiv.results.description.ResultDescription;
 import org.atlasapi.equiv.results.scores.ScoredCandidates;
+import org.atlasapi.media.entity.Content;
 
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
 
-public class EquivalenceGenerators<T> {
+public class EquivalenceGenerators<T extends Content> {
     
-    public static <T> EquivalenceGenerators<T> from(Iterable<? extends EquivalenceGenerator<T>> generators) {
-        return new EquivalenceGenerators<T>(generators);
+    public static <T extends Content> EquivalenceGenerators<T> from(Iterable<? extends EquivalenceGenerator<T>> generators, Set<String> excludedUris) {
+        return new EquivalenceGenerators<T>(generators, excludedUris);
     }
 
     private final List<? extends EquivalenceGenerator<T>> generators;
-    
-    public EquivalenceGenerators(Iterable<? extends EquivalenceGenerator<T>> generators) {
+    private final Set<String> excludedUris;
+
+    public EquivalenceGenerators(Iterable<? extends EquivalenceGenerator<T>> generators, Set<String> excludedUris) {
         this.generators = ImmutableList.copyOf(generators);
+        this.excludedUris = excludedUris;
     }
     
     public List<ScoredCandidates<T>> generate(T content, ResultDescription desc) {
         desc.startStage("Generating equivalences");
         Builder<ScoredCandidates<T>> generatedScores = ImmutableList.builder();
-        
+
+
+        if (excludedUris.contains(content.getCanonicalUri())) {
+            desc.appendText("Content %s is in equivalence blacklist and will not be equivalated",
+                    content.getCanonicalUri());
+            return generatedScores.build();
+        }
+
         for (EquivalenceGenerator<T> generator : generators) {
             try {
                 desc.startStage(generator.toString());
---
+++ src/test/java/org/atlasapi/equiv/generators/EquivalenceGeneratorsTest.java
@@ -0,0 +1,60 @@
+package org.atlasapi.equiv.generators;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.atlasapi.equiv.results.description.ResultDescription;
+import org.atlasapi.equiv.results.scores.ScoredCandidates;
+import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.Item;
+
+import com.google.common.collect.ImmutableSet;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EquivalenceGeneratorsTest {
+
+    private EquivalenceGenerators<Content> generators;
+    private Set<String> excludedUris;
+    @Mock
+    private EquivalenceGenerator<Content> generator;
+    @Mock
+    private ResultDescription resultDescription;
+    @Mock
+    private ScoredCandidates candidates;
+
+    @Before
+    public void setUp() {
+        excludedUris = ImmutableSet.of("excluded");
+        generators = EquivalenceGenerators.from(Collections.singleton(generator), excludedUris);
+        when(generator.generate(any(Content.class), any(ResultDescription.class))).thenReturn(candidates);
+    }
+
+    @Test
+    public void testGeneratorExcludesUriFromTheList() {
+        Item item = new Item();
+        item.setCanonicalUri("excluded");
+        List<ScoredCandidates<Content>> generated = generators.generate(item, resultDescription);
+        assertTrue(generated.isEmpty());
+    }
+
+    @Test
+    public void testGeneratorNotExcludesUriFromTheList() {
+        Item item = new Item();
+        item.setCanonicalUri("notexcluded");
+        generators.generate(item, resultDescription);
+        verify(generator).generate(item, resultDescription);
+    }
+
+}

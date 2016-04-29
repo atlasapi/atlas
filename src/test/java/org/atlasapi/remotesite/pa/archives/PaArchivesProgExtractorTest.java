20ea5940d7247e521bf59499b1e1510d281ca3c2
Dias Saparov <dias@metabroadcast.com>
2016-04-29 10:54:13 +0100

    fixing test due to changes in film ingesting

--- src/test/java/org/atlasapi/remotesite/pa/archives/PaArchivesProgExtractorTest.java
+++ src/test/java/org/atlasapi/remotesite/pa/archives/PaArchivesProgExtractorTest.java
@@ -70,6 +70,7 @@ public class PaArchivesProgExtractorTest {
     public void setUp() throws URISyntaxException {
         when(resolvedContent.getFirstValue()).thenReturn(Maybe.<Identified>nothing());
         when(resolver.findByCanonicalUris(anyCollection())).thenReturn(resolvedContent);
+        when(resolver.findByUris(anyCollection())).thenReturn(resolvedContent);
         progProcessor = new PaProgrammeProcessor(writer,resolver,log,paTagMap);
         transformer = new PaDataToUpdatesTransformer();
     }
@@ -103,19 +104,19 @@ public class PaArchivesProgExtractorTest {
 
     @Test
     public void testIngestHierarchyFromProgdata() throws Exception {
-        DateTime dateTime = DateTime.now();
+        DateTime dateTime = DateTime.now(DateTimeZone.UTC);
         ProgData archives = generateArchiveProgdata();
         org.atlasapi.remotesite.pa.listings.bindings.ProgData listing = transformer.transformToListingProgdata(archives);
         ContentHierarchyWithoutBroadcast hierarchy = progProcessor.process(listing, DateTimeZone.UTC, Timestamp.of(dateTime)).get();
         assertThat(hierarchy.getBrand(), is(Optional.<Brand>absent()));
         assertThat(hierarchy.getSeries(), is(Optional.<Series>absent()));
         Item item = hierarchy.getItem();
-        Set<Alias> aliases = ImmutableSet.of(new Alias("pa:film", "5217"), new Alias("rt:filmid", "5217"), new Alias("gb:pressassociation:prod:prog_id", "263544"));
+        Set<Alias> aliases = ImmutableSet.of(new Alias("pa:film", "263544"), new Alias("rt:filmid", "5217"), new Alias("gb:pressassociation:prod:prog_id", "263544"));
         assertThat(item.getAliases(), is(aliases));
         Set<String> aliasUrls = ImmutableSet.of("http://pressassociation.com/263544");
         assertThat(item.getAliasUrls(), is(aliasUrls));
         assertThat(item.getBlackAndWhite(), is(false));
-        assertThat(item.getCanonicalUri(), is("http://pressassociation.com/films/5217"));
+        assertThat(item.getCanonicalUri(), is("http://pressassociation.com/episodes/263544"));
         assertThat(item.getDescription(), is("Sci-fi adventure sequel, with Harrison Ford, Mark Hamill, Carrie Fisher, Billy Dee Williams, Alec Guinness and David Prowse."));
         Set<String> genres = ImmutableSet.of("http://ref.atlasapi.org/genres/atlas/drama", "http://pressassociation.com/genres/1300");
         assertThat(item.getGenres(), is(genres));

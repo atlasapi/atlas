4d8f5c3702d68ebe1a3216e125ceb552c457399c
Dias Saparov <dias@metabroadcast.com>
2016-05-19 16:49:03 +0100

    masterbrand and service saved with different uri prefix

--- src/main/java/org/atlasapi/remotesite/bbc/nitro/GlycerinNitroChannelAdapter.java
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/GlycerinNitroChannelAdapter.java
@@ -5,15 +5,14 @@ import java.util.List;
 import org.atlasapi.media.channel.Channel;
 import org.atlasapi.media.channel.ChannelType;
 import org.atlasapi.media.entity.Alias;
-import org.atlasapi.media.entity.Image;
 import org.atlasapi.media.entity.MediaType;
 import org.atlasapi.media.entity.Publisher;
 
 import com.metabroadcast.atlas.glycerin.Glycerin;
 import com.metabroadcast.atlas.glycerin.GlycerinException;
 import com.metabroadcast.atlas.glycerin.GlycerinResponse;
-import com.metabroadcast.atlas.glycerin.model.Brand;
 import com.metabroadcast.atlas.glycerin.model.Id;
+import com.metabroadcast.atlas.glycerin.model.Ids;
 import com.metabroadcast.atlas.glycerin.model.MasterBrand;
 import com.metabroadcast.atlas.glycerin.model.Service;
 import com.metabroadcast.atlas.glycerin.queries.MasterBrandsMixin;
@@ -35,7 +34,8 @@ public class GlycerinNitroChannelAdapter implements NitroChannelAdapter {
     private static final String BBC_SERVICE_PID = "bbc:service:pid";
     private static final String TERRESTRIAL_SERVICE_LOCATOR = "terrestrial_service_locator";
     private static final String BBC_SERVICE_LOCATOR = "bbc:service:locator";
-    private static final String NITRO_URI_PREFIX = "http://nitro.bbc.co.uk/";
+    private static final String NITRO_SERVICE_URI_PREFIX = "http://nitro.bbc.co.uk/service/";
+    private static final String NITRO_MASTERBRAND_URI_PREFIX = "http://nitro.bbc.co.uk/masterbrand/";
 
     private final Glycerin glycerin;
 
@@ -56,15 +56,27 @@ public class GlycerinNitroChannelAdapter implements NitroChannelAdapter {
         while (!exhausted) {
             List<Service> results = paginateServices(startingPoint);
             for (Service result : results) {
-                Channel channel = getChannel(result);
-
-                channels.add(channel);
+                Ids ids = result.getIds();
+                if (ids != null) {
+                    generateAndAddChannelsFromLocators(channels, result, ids);
+                }
             }
             startingPoint ++;
             exhausted = results.size() != MAXIMUM_PAGE_SIZE;
         }
 
         return channels.build();
+    }
+
+    private void generateAndAddChannelsFromLocators(ImmutableSet.Builder<Channel> channels,
+            Service result, Ids ids) {
+        Channel channel;
+        for (Id id : ids.getId()) {
+            if (id.getType().equals(TERRESTRIAL_SERVICE_LOCATOR)) {
+                channel = getChannelWithLocatorAlias(result, id);
+                channels.add(channel);
+            }
+        }
     }
 
     @Override
@@ -115,18 +127,17 @@ public class GlycerinNitroChannelAdapter implements NitroChannelAdapter {
         Channel.Builder builder = Channel.builder()
                 .withBroadcaster(Publisher.BBC)
                 .withSource(Publisher.BBC_NITRO)
-                .withUri(NITRO_URI_PREFIX + result.getMid())
+                .withUri(NITRO_MASTERBRAND_URI_PREFIX + result.getMid())
                 .withChannelType(ChannelType.MASTERBRAND);
 
-        Brand.Images images = result.getImages();
-        if (images != null) {
-            builder.withImage(new Image(images.getImage().getTemplateUrl()));
-        }
-
-        if (result.getName() != null) {
-            builder.withTitle(result.getName());
+        String name = result.getName();
+        if (name != null) {
+            builder.withTitle(name);
+            inferAndSetMediaType(builder, name);
         } else {
-            builder.withTitle(result.getTitle());
+            String title = result.getTitle();
+            inferAndSetMediaType(builder, title);
+            builder.withTitle(title);
         }
 
         if (result.getSynopses() != null) {
@@ -146,6 +157,14 @@ public class GlycerinNitroChannelAdapter implements NitroChannelAdapter {
         return builder.build();
     }
 
+    private void inferAndSetMediaType(Channel.Builder builder, String name) {
+        if (name.toLowerCase().contains("music") || name.toLowerCase().contains("radio")) {
+            builder.withMediaType(MediaType.AUDIO);
+        } else {
+            builder.withMediaType(MediaType.VIDEO);
+        }
+    }
+
     private Channel getChannel(Service result) {
         Channel.Builder builder = Channel.builder()
                 .withBroadcaster(Publisher.BBC)
@@ -154,7 +173,6 @@ public class GlycerinNitroChannelAdapter implements NitroChannelAdapter {
                 .withTitle(result.getName())
                 .withMediumDescription(result.getDescription())
                 .withRegion(result.getRegion())
-                .withUri(NITRO_URI_PREFIX + result.getSid())
                 .withChannelType(ChannelType.CHANNEL);
         Optional<LocalDate> startDate = getStartDate(result);
         if (startDate.isPresent()) {
@@ -169,11 +187,16 @@ public class GlycerinNitroChannelAdapter implements NitroChannelAdapter {
             if (id.getType().equals(PID)) {
                 channel.addAlias(new Alias(BBC_SERVICE_PID, id.getValue()));
             }
-            if (id.getType().equals(TERRESTRIAL_SERVICE_LOCATOR)) {
-                channel.addAlias(new Alias(BBC_SERVICE_LOCATOR, id.getValue()));
-                channel.addAliasUrl(id.getValue());
-            }
         }
+        return channel;
+    }
+
+    private Channel getChannelWithLocatorAlias(Service result, Id locator) {
+        Channel channel = getChannel(result);
+        String locatorValue = locator.getValue();
+        channel.setCanonicalUri(locatorValue);
+        channel.setAliases(ImmutableSet.of(new Alias(BBC_SERVICE_LOCATOR, locatorValue)));
+        channel.setAliasUrls(ImmutableSet.of(locatorValue));
         return channel;
     }
 

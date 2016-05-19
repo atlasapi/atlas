09f19932d9ce47ff756bb03c57587d53ae562a10
Dias Saparov <dias@metabroadcast.com>
2016-05-19 16:37:06 +0100

    added merging channels after ingest and before writing

--- src/main/java/org/atlasapi/remotesite/bbc/nitro/ChannelIngestTask.java
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/ChannelIngestTask.java
@@ -1,13 +1,16 @@
 package org.atlasapi.remotesite.bbc.nitro;
 
 import org.atlasapi.media.channel.Channel;
+import org.atlasapi.media.channel.ChannelResolver;
 import org.atlasapi.media.channel.ChannelWriter;
 
 import com.metabroadcast.atlas.glycerin.GlycerinException;
+import com.metabroadcast.common.base.Maybe;
 import com.metabroadcast.common.scheduling.ScheduledTask;
 
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,14 +22,16 @@ public class ChannelIngestTask extends ScheduledTask {
 
     private final NitroChannelAdapter channelAdapter;
     private final ChannelWriter channelWriter;
+    private final ChannelResolver channelResolver;
 
-    private ChannelIngestTask(NitroChannelAdapter channelAdapter, ChannelWriter channelWriter) {
+    private ChannelIngestTask(NitroChannelAdapter channelAdapter, ChannelWriter channelWriter, ChannelResolver channelResolver) {
         this.channelAdapter = checkNotNull(channelAdapter);
         this.channelWriter = checkNotNull(channelWriter);
+        this.channelResolver = checkNotNull(channelResolver);
     }
 
-    public static ChannelIngestTask create(NitroChannelAdapter channelAdapter, ChannelWriter channelWriter) {
-        return new ChannelIngestTask(channelAdapter, channelWriter);
+    public static ChannelIngestTask create(NitroChannelAdapter channelAdapter, ChannelWriter channelWriter, ChannelResolver channelResolver) {
+        return new ChannelIngestTask(channelAdapter, channelWriter, channelResolver);
     }
 
     @Override
@@ -40,14 +45,14 @@ public class ChannelIngestTask extends ScheduledTask {
 
             reportStatus("Writing channels");
             int failedServices = 0;
-            writeChannels(services, failedServices);
+            writeAndMergeChannels(services, failedServices);
             int servicesCount = services.size();
             reportStatus(String.format("%d services failed out of %d", failedServices,
                     servicesCount
             ));
 
             int failedMasterbrands = 0;
-            writeChannels(masterbrands, failedMasterbrands);
+            writeAndMergeChannels(masterbrands, failedMasterbrands);
             int masterbrandsCount = masterbrands.size();
             reportStatus(String.format("%d masterbrands failed out of %d", failedMasterbrands,
                     masterbrandsCount
@@ -61,11 +66,36 @@ public class ChannelIngestTask extends ScheduledTask {
 
     }
 
-    private void writeChannels(Iterable<Channel> channels, int failedCount) {
-
+    private void writeAndMergeChannels(Iterable<Channel> channels, int failedCount) {
         for (Channel channel : channels) {
+            Maybe<Channel> existing = channelResolver.fromUri(channel.getCanonicalUri());
             try {
-                channelWriter.createOrUpdate(channel);
+                if (existing.hasValue()) {
+                    Channel existingChannel = existing.requireValue();
+
+                    existingChannel.setTitles(channel.getAllTitles());
+                    existingChannel.setAdult(channel.getAdult());
+                    existingChannel.setImages(channel.getAllImages());
+                    existingChannel.setStartDate(channel.getStartDate());
+                    existingChannel.setEndDate(channel.getEndDate());
+                    existingChannel.setRelatedLinks(channel.getRelatedLinks());
+                    existingChannel.addAliasUrls(channel.getAliasUrls());
+                    existingChannel.setParent(channel.getParent());
+                    existingChannel.setMediaType(channel.getMediaType());
+                    existingChannel.setHighDefinition(channel.getHighDefinition());
+                    existingChannel.setRegional(channel.getRegional());
+                    existingChannel.setTimeshift(channel.getTimeshift());
+                    existingChannel.setGenres(channel.getGenres());
+                    existingChannel.setAvailableFrom(Sets.union(existingChannel.getAvailableFrom(), channel.getAvailableFrom()));
+                    existingChannel.setShortDescription(channel.getShortDescription());
+                    existingChannel.setMediumDescription(channel.getMediumDescription());
+                    existingChannel.setLongDescription(channel.getLongDescription());
+                    existingChannel.setChannelType(channel.getChannelType());
+                    existingChannel.setRegion(channel.getRegion());
+                    channelWriter.createOrUpdate(existingChannel);
+                } else {
+                    channelWriter.createOrUpdate(channel);
+                }
             } catch (Exception e) {
                 log.error("Failed to write channel {}", channel.getCanonicalUri());
                 failedCount ++;

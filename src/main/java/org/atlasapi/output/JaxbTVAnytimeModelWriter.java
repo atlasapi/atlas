699f70c45b8155d37a66ce647a1d4568a926db8f
Jamie Perkins <jamie@metabraodcast.com>
2016-06-01 19:01:49 +0100

    Revert "Strip nil tags from XML output"
    This reverts commit f7c8d13b1f549418261cb4e58f0b828dc047adc1.

--- src/main/java/org/atlasapi/output/JaxbTVAnytimeModelWriter.java
+++ src/main/java/org/atlasapi/output/JaxbTVAnytimeModelWriter.java
@@ -1,6 +1,5 @@
 package org.atlasapi.output;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
@@ -43,20 +42,17 @@ public class JaxbTVAnytimeModelWriter implements AtlasModelWriter<JAXBElement<TV
     
     private void writeOut(final HttpServletRequest request, final HttpServletResponse response, 
             final JAXBElement<TVAMainType> result) throws JAXBException, IOException {
-
-        OutputStream out = response.getOutputStream();
+        
         JAXBContext context = JAXBContext.newInstance("tva.metadata._2010");
         Marshaller marshaller = context.createMarshaller();
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        marshaller.marshal(response, baos);
+        OutputStream out = response.getOutputStream();
         String accepts = request.getHeader(HttpHeaders.ACCEPT_ENCODING);
         if (accepts != null && accepts.contains(GZIP_HEADER_VALUE)) {
             response.setHeader(HttpHeaders.CONTENT_ENCODING, GZIP_HEADER_VALUE);
             out = new GZIPOutputStream(out);
         }
         try {
-            /* TODO FIXME This output hack was put it to support weirdness from YouView */
-            out.write(baos.toString().replace(" xsi:nil=\"true\"", "").getBytes());
+            marshaller.marshal(result, out);
         } finally {
             if (out instanceof GZIPOutputStream) {
                 ((GZIPOutputStream) out).finish();

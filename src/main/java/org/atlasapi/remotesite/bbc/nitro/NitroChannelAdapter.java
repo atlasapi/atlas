bd4e6720838f3071c5c397564955e9112ed7ae4d
Jamie Perkins <mail@jperkins.co.uk>
2016-06-02 17:22:40 +0100

    Refactor the way parent channels are resolved for Nitro ingest

--- src/main/java/org/atlasapi/remotesite/bbc/nitro/NitroChannelAdapter.java
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/NitroChannelAdapter.java
@@ -1,5 +1,6 @@
 package org.atlasapi.remotesite.bbc.nitro;
 
+import com.google.common.collect.ImmutableMap;
 import org.atlasapi.media.channel.Channel;
 
 import com.metabroadcast.atlas.glycerin.GlycerinException;
@@ -7,6 +8,8 @@ import com.metabroadcast.atlas.glycerin.GlycerinException;
 import com.google.common.collect.ImmutableSet;
 
 public interface NitroChannelAdapter {
+
+    ImmutableSet<Channel> fetchServices(ImmutableMap<String, Channel> build) throws GlycerinException;
 
     ImmutableSet<Channel> fetchServices() throws GlycerinException;
 

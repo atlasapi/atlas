3bbc1cd03b049046de9de3c821724156f85644c6
Jamie Perkins <mail@jperkins.co.uk>
2016-06-08 16:05:36 +0100

    Tidy up AtlasMain

--- src/main/java/org/atlasapi/AtlasMain.java
+++ src/main/java/org/atlasapi/AtlasMain.java
@@ -1,27 +1,17 @@
 package org.atlasapi;
 
-import java.io.File;
-import java.lang.management.ManagementFactory;
-import java.lang.reflect.InvocationTargetException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
-import java.security.ProtectionDomain;
-import java.text.DecimalFormat;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.Executor;
-import java.util.concurrent.TimeUnit;
-
+import com.codahale.metrics.Histogram;
 import com.codahale.metrics.JvmAttributeGaugeSet;
 import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
-import com.codahale.metrics.jvm.BufferPoolMetricSet;
 import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
 import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
 import com.google.api.client.repackaged.com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
 import org.atlasapi.util.jetty.InstrumentedQueuedThreadPool;
 import org.eclipse.jetty.io.ByteBufferPool;
 import org.eclipse.jetty.server.Connector;
@@ -33,10 +23,17 @@ import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.util.thread.Scheduler;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-import com.codahale.metrics.Histogram;
-import com.codahale.metrics.MetricRegistry;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.lang.reflect.InvocationTargetException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.security.ProtectionDomain;
+import java.text.DecimalFormat;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
 public class AtlasMain {
 
@@ -45,23 +42,23 @@ public class AtlasMain {
     private static final int DEFAULT_SERVER_REQUEST_THREADS = 100;
     private static final String SERVER_REQUEST_THREAD_PREFIX = "api-request-thread";
     private static final int SERVER_ACCEPT_QUEUE_SIZE = 200;
-    
+
     private static final String SERVER_PORT_OVERRIDE_PROPERTY_NAME = "server.port";
     private static final int API_DEFAULT_PORT = 8080;
     private static final int PROCESSING_DEFAULT_PORT = 8282;
-    
+
     private static final int MONITORING_REQUEST_THREADS = 20;
     private static final String MONITORING_REQUEST_THREAD_PREFIX = "monitoring-request-thread";
     private static final int MONITORING_DEFAULT_PORT = 8081;
     private static final String MONITORING_PORT_OVERRIDE_PROPERTY_NAME = "monitoring.port";
-    
+
     public static final String CONTEXT_ATTRIBUTE = "ATLAS_MAIN";
     private static final String LOCAL_WAR_DIR = "./src/main/webapp";
 
     private static final boolean IS_PROCESSING = Boolean.parseBoolean(
-                                                    System.getProperty("processing.config")
-                                                 );
-    
+            System.getProperty("processing.config")
+    );
+
     private static final String SAMPLING_PERIOD_PROPERTY = "samplingPeriodMinutes";
     private static final int DEFAULT_SAMPLING_PERIOD_MINUTES = 3;
     public static final InetSocketAddress GRAPHITE_ADDRESS = new InetSocketAddress("graphite.mbst.tv", 2003);
@@ -77,12 +74,12 @@ public class AtlasMain {
     }
 
     public void start() throws Exception {
-       
         WebAppContext apiContext = createWebApp(warBase() + "/WEB-INF/web.xml", createApiServer());
         apiContext.setAttribute(CONTEXT_ATTRIBUTE, this);
         if (!IS_PROCESSING) {
-            WebAppContext monitoringContext = createWebApp(warBase() + "/WEB-INF/web-monitoring.xml",
-                    createMonitoringServer());
+            WebAppContext monitoringContext = createWebApp(
+                    warBase() + "/WEB-INF/web-monitoring.xml", createMonitoringServer()
+            );
             monitoringContext.setAttribute(CONTEXT_ATTRIBUTE, this);
         }
     }
@@ -92,7 +89,6 @@ public class AtlasMain {
         ctx.setDescriptor(descriptor);
         server.setHandler(ctx);
         server.start();
-        
         return ctx;
     }
 
@@ -106,66 +102,98 @@ public class AtlasMain {
 
     private Server createApiServer() throws Exception {
         int requestThreads;
-        String requestThreadsString = System.getProperty(SERVER_REQUEST_THREADS_OVERRIDE_PROPERTY_NAME);
+        String requestThreadsString = System.getProperty(
+                SERVER_REQUEST_THREADS_OVERRIDE_PROPERTY_NAME
+        );
         if (requestThreadsString == null) {
             requestThreads = DEFAULT_SERVER_REQUEST_THREADS;
         } else {
             requestThreads = Integer.parseInt(requestThreadsString);
         }
 
-        return createServer(defaultPort(), SERVER_PORT_OVERRIDE_PROPERTY_NAME, requestThreads, 
-                SERVER_ACCEPT_QUEUE_SIZE, SERVER_REQUEST_THREAD_PREFIX);
+        return createServer(
+                defaultPort(),
+                SERVER_PORT_OVERRIDE_PROPERTY_NAME,
+                requestThreads,
+                SERVER_ACCEPT_QUEUE_SIZE,
+                SERVER_REQUEST_THREAD_PREFIX
+        );
     }
 
     private Server createMonitoringServer() throws Exception {
         int defaultAcceptQueueSize = 0;
-        return createServer(MONITORING_DEFAULT_PORT, MONITORING_PORT_OVERRIDE_PROPERTY_NAME, 
-                MONITORING_REQUEST_THREADS, defaultAcceptQueueSize, MONITORING_REQUEST_THREAD_PREFIX);
+        return createServer(
+                MONITORING_DEFAULT_PORT,
+                MONITORING_PORT_OVERRIDE_PROPERTY_NAME,
+                MONITORING_REQUEST_THREADS,
+                defaultAcceptQueueSize,
+                MONITORING_REQUEST_THREAD_PREFIX
+        );
     }
 
-    private Server createServer(int defaultPort, String portPropertyName, int maxThreads,
-            int acceptQueueSize, String threadNamePrefix) {
-         
+    private Server createServer(
+            int defaultPort,
+            String portPropertyName,
+            int maxThreads,
+            int acceptQueueSize,
+            String threadNamePrefix
+    ) {
         Server server = new Server(createRequestThreadPool(maxThreads, threadNamePrefix));
-        createServerConnector(server, createHttpConnectionFactory(), defaultPort, portPropertyName, 
-                acceptQueueSize);
-
+        createServerConnector(
+                server,
+                createHttpConnectionFactory(),
+                defaultPort,
+                portPropertyName,
+                acceptQueueSize
+        );
         return server;
     }
-    
+
     private QueuedThreadPool createRequestThreadPool(int maxThreads, String threadNamePrefix) {
-        QueuedThreadPool pool = new InstrumentedQueuedThreadPool(metrics, getSamplingPeriod(), 
-                maxThreads);
+        QueuedThreadPool pool = new InstrumentedQueuedThreadPool(
+                metrics,
+                getSamplingPeriod(),
+                maxThreads
+        );
         pool.setName(threadNamePrefix);
-        
         return pool;
     }
 
-    private void createServerConnector(Server server, HttpConnectionFactory connectionFactory,
-            int defaultPort, String portPropertyName, int acceptQueueSize) {
-        
+    private void createServerConnector(
+            Server server,
+            HttpConnectionFactory connectionFactory,
+            int defaultPort,
+            String portPropertyName,
+            int acceptQueueSize
+    ) {
         int acceptors = Runtime.getRuntime().availableProcessors();
         Executor defaultExecutor = null;
         Scheduler defaultScheduler = null;
         ByteBufferPool defaultByteBufferPool = null;
         int selectors = 0;
-        
-        ServerConnector connector = new ServerConnector(server, defaultExecutor, defaultScheduler, 
-                defaultByteBufferPool, acceptors, selectors, connectionFactory);
-        
+
+        ServerConnector connector = new ServerConnector(
+                server,
+                defaultExecutor,
+                defaultScheduler,
+                defaultByteBufferPool,
+                acceptors,
+                selectors,
+                connectionFactory
+        );
+
         connector.setPort(getPort(defaultPort, portPropertyName));
         connector.setAcceptQueueSize(acceptQueueSize);
-        server.setConnectors(new Connector[] { connector });
+        server.setConnectors(new Connector[]{connector});
     }
-    
+
     private HttpConnectionFactory createHttpConnectionFactory() {
         HttpConfiguration config = new HttpConfiguration();
         config.setRequestHeaderSize(8192);
         config.setResponseHeaderSize(1024);
-        
         return new HttpConnectionFactory(config);
     }
-    
+
     private int getPort(int defaultPort, String portProperty) {
         String customPort = System.getProperty(portProperty);
         if (customPort != null) {
@@ -173,7 +201,7 @@ public class AtlasMain {
         }
         return defaultPort;
     }
-    
+
     private int getSamplingPeriod() {
         String customSamplingDuration = System.getProperty(SAMPLING_PERIOD_PROPERTY);
         if (customSamplingDuration != null) {
@@ -187,21 +215,30 @@ public class AtlasMain {
     }
 
     @SuppressWarnings("unchecked")
-    public static Map<String, String> getMetrics(Object atlasMain)
-            throws IllegalArgumentException,
-            SecurityException, IllegalAccessException, InvocationTargetException {
+    public static Map<String, String> getMetrics(
+            Object atlasMain
+    ) throws
+            IllegalArgumentException,
+            SecurityException,
+            IllegalAccessException,
+            InvocationTargetException {
+
         Class<? extends Object> clazz = atlasMain.getClass();
         if (clazz.getCanonicalName() != AtlasMain.class.getCanonicalName()) {
-            throw new IllegalArgumentException("Parameter must be instance of "
-                + AtlasMain.class.getCanonicalName());
+            throw new IllegalArgumentException(
+                    "Parameter must be instance of " + AtlasMain.class.getCanonicalName()
+            );
         }
-
         try {
-            return (Map<String, String>) clazz.getDeclaredMethod(METRIC_METHOD_NAME).invoke(atlasMain);
+            return (Map<String, String>) clazz.getDeclaredMethod(METRIC_METHOD_NAME)
+                    .invoke(atlasMain);
         } catch (NoSuchMethodException e) {
             throw new IllegalArgumentException(
-                    "Couldn't find method " + METRIC_METHOD_NAME + ": Perhaps a mismatch between AtlasMain objects across classloaders?",
-                    e);
+                    "Couldn't find method " +
+                            METRIC_METHOD_NAME +
+                            ": Perhaps a mismatch between AtlasMain objects across classloaders?",
+                    e
+            );
         }
     }
 
@@ -219,8 +256,10 @@ public class AtlasMain {
                     .convertDurationsTo(TimeUnit.MILLISECONDS)
                     .filter(MetricFilter.ALL)
                     .build(new Graphite(GRAPHITE_ADDRESS));
-            reporter.start(30, TimeUnit.SECONDS);
-            System.out.println("Started Graphite reporter");
+            if (!IS_PROCESSING) {
+                reporter.start(30, TimeUnit.SECONDS);
+                System.out.println("Started Graphite reporter");
+            }
             return reporter;
         } catch (Exception e) {
             throw Throwables.propagate(e);

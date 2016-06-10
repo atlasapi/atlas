c1592979b3f97a2175bb031788fd867abe73fdb4
Jamie Perkins <mail@jperkins.co.uk>
2016-06-10 17:56:02 +0100

    Revert "Revert "Merge branch 'feature/processingTime' into stage""
    This reverts commit 9718879efbfe50718ab32ecea1726d4c5953ef73.

---
+++ src/main/java/org/atlasapi/query/ProcessingTimeFilter.java
@@ -0,0 +1,64 @@
+package org.atlasapi.query;
+
+import java.io.IOException;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.common.annotations.VisibleForTesting;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * This keeps track of how long it takes to process a request and adds it as a response header.
+ * It is intended to give us a metric of how long it takes to process a request that does not
+ * include any time waiting for a request thread to become available.
+ */
+public class ProcessingTimeFilter extends OncePerRequestFilter {
+
+    public static final String PROCESSING_TIME_HEADER = "Processing-Time";
+
+    private final Clock clock;
+
+    public ProcessingTimeFilter() {
+        this(Clock.systemUTC());
+    }
+
+    private ProcessingTimeFilter(Clock clock) {
+        this.clock = checkNotNull(clock);
+    }
+
+    @VisibleForTesting
+    static ProcessingTimeFilter create(Clock clock) {
+        return new ProcessingTimeFilter(clock);
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        ZonedDateTime start = ZonedDateTime.now(clock);
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            ZonedDateTime end = ZonedDateTime.now(clock);
+
+            response.addHeader(
+                    PROCESSING_TIME_HEADER,
+                    String.valueOf(
+                            Duration.between(start, end).toMillis()
+                    )
+            );
+        }
+    }
+}

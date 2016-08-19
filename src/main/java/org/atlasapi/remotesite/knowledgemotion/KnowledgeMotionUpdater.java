01daf5bf2136b54d8157b1fef1ecb43557873c6f
Martins Irbe <martins@ip-10-0-1-111.eu-west-1.compute.internal>
2016-08-19 11:47:36 +0100

    Fixing compilation errors caused by common ingest changes.
    Updated Knowledge Motion reporting to include raw file.

--- src/main/java/org/atlasapi/remotesite/knowledgemotion/KnowledgeMotionUpdater.java
+++ src/main/java/org/atlasapi/remotesite/knowledgemotion/KnowledgeMotionUpdater.java
@@ -10,11 +10,16 @@ import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.content.ContentCategory;
 import org.atlasapi.persistence.content.listing.ContentLister;
 import org.atlasapi.persistence.content.listing.ContentListingCriteria;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Sets;
+
+import com.metabroadcast.common.ingest.monitorclient.model.Entity;
 import com.metabroadcast.common.ingest.s3.process.ProcessingResult;
 
 public class KnowledgeMotionUpdater {
@@ -24,6 +29,7 @@ public class KnowledgeMotionUpdater {
     private final KnowledgeMotionDataRowHandler dataHandler;
     private final Iterable<KnowledgeMotionSourceConfig> allKmPublishers;
     private final ContentLister contentLister;
+    private final ObjectMapper mapper;
 
     private Set<String> seenUris;
 
@@ -35,13 +41,15 @@ public class KnowledgeMotionUpdater {
         this.contentLister = checkNotNull(contentLister);
 
         seenUris = Sets.newHashSet();
+
+        this.mapper = new ObjectMapper();
     }
 
     protected ProcessingResult.Builder process(Iterator<KnowledgeMotionDataRow> rows,
             ProcessingResult.Builder resultBuilder) {
         if (!rows.hasNext()) {
             log.info("Knowledgemotion Common Ingest received an empty file");
-            resultBuilder.error("input file", "Empty file");
+            resultBuilder.error(String.format("Empty input file %s"));
         }
 
         KnowledgeMotionDataRow firstRow = rows.next();
@@ -54,14 +62,16 @@ public class KnowledgeMotionUpdater {
         }
         if (publisher == null) {
             StringBuilder errorText = new StringBuilder();
-            errorText.append("First row did not contain a recognised publisher in the 'Source' column.").append("\n");
+            errorText.append(
+                    "First row did not contain a recognised publisher in the 'Source' column."
+            ).append("\n");
             errorText.append("Found: " + publisherRowHeader).append("\n");
             errorText.append("Valid publishers are: ").append("\n");
             for (KnowledgeMotionSourceConfig config : allKmPublishers) {
                 errorText.append(config.rowHeader()).append("\n");
             }
 
-            resultBuilder.error("input file", errorText.toString());
+            resultBuilder.error(String.format("input file %s",  errorText.toString()));
         }
 
         boolean allRowsSuccess = true;
@@ -79,7 +89,11 @@ public class KnowledgeMotionUpdater {
          * unpublish everything else by this publisher
          */
         if (allRowsSuccess) {
-            Iterator<Content> allStoredKmContent = contentLister.listContent(ContentListingCriteria.defaultCriteria().forContent(ContentCategory.TOP_LEVEL_ITEM).forPublisher(publisher).build());
+            Iterator<Content> allStoredKmContent = contentLister.listContent(
+                    ContentListingCriteria.defaultCriteria()
+                            .forContent(ContentCategory.TOP_LEVEL_ITEM)
+                            .forPublisher(publisher).build()
+            );
             while (allStoredKmContent.hasNext()) {
                 Content item = allStoredKmContent.next();
                 if (!seenUris.contains(item.getCanonicalUri())) {
@@ -102,11 +116,33 @@ public class KnowledgeMotionUpdater {
                 seenUris.add(written.get().getCanonicalUri());
             }
             log.debug("Successfully updated row {}", row.getId());
-            resultBuilder.success();
+            resultBuilder.addEntity(Entity.success()
+                    .withId(row.getId())
+                    .withRaw(mapper.writeValueAsString(row))
+                    .build()
+            );
             return true;
         } catch (RuntimeException e) {
             log.debug("Failed to update row {}", row.getId(), e);
-            resultBuilder.error(row.getId(), "While merging content: " + e.getMessage());
+            resultBuilder.addEntity(Entity.failure()
+                    .withId(row.getId())
+                    .withError(String.format(
+                            "While merging content: %s \n%s",
+                            row.getId(),
+                            e.getMessage()
+                    ))
+                    .build());
+            return false;
+        } catch (JsonProcessingException e) {
+            log.debug("Failed to convert Knowledge Motion row to a JSON. ", row.getId(), e);
+            resultBuilder.addEntity(Entity.failure()
+                    .withId(row.getId())
+                    .withError(String.format(
+                            "While converting Knowledge Motion row to a JSON object: %s \n%s",
+                            row.getId(),
+                            e.getMessage()
+                    ))
+                    .build());
             return false;
         }
     }

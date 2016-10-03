bb021454d416821188b2096e8b1bcfa8f8e65d77
Emils Solmanis <emils@metabroadcast.com>
2016-10-03 16:58:23 +0100

    Actually ingest channels with no DVB locators

--- src/main/java/org/atlasapi/remotesite/bbc/nitro/NitroChannelAdapter.java
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/NitroChannelAdapter.java
@@ -1,17 +1,18 @@
 package org.atlasapi.remotesite.bbc.nitro;
 
-import com.google.common.collect.ImmutableMap;
 import org.atlasapi.media.channel.Channel;
 
 import com.metabroadcast.atlas.glycerin.GlycerinException;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 public interface NitroChannelAdapter {
 
-    ImmutableSet<Channel> fetchServices(ImmutableMap<String, Channel> build) throws GlycerinException;
+    ImmutableList<Channel> fetchServices(ImmutableMap<String, Channel> build) throws GlycerinException;
 
-    ImmutableSet<Channel> fetchServices() throws GlycerinException;
+    ImmutableList<Channel> fetchServices() throws GlycerinException;
 
     ImmutableSet<Channel> fetchMasterbrands() throws GlycerinException;
 

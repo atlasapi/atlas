40a679105061abb6cccf47f5de66521b49d2be4b
Emils Solmanis <emils@metabroadcast.com>
2016-10-13 11:34:17 +0100

    Add support to hard-code regions per DVB locator when hard-coding multiple locators

---
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/channels/hax/LocatorWithRegions.java
@@ -0,0 +1,33 @@
+package org.atlasapi.remotesite.bbc.nitro.channels.hax;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LocatorWithRegions {
+
+    private final String locator;
+    private final List<String> regions;
+
+    public static LocatorWithRegions create(
+            @JsonProperty("locator") String locator,
+            @JsonProperty("regions") List<String> regions
+    ) {
+        return new LocatorWithRegions(locator, regions);
+    }
+
+    private LocatorWithRegions(String locator, List<String> regions) {
+        this.locator = locator;
+        this.regions = regions;
+    }
+
+    @JsonProperty("locator")
+    public String getLocator() {
+        return locator;
+    }
+
+    @JsonProperty("regions")
+    public List<String> getRegions() {
+        return regions;
+    }
+}

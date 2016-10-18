752b51229ad00ca5e25bb64abdffc3c6ce181f81
Jamie Coleman <jamiec@metabroadcast.com>
2016-10-18 18:55:40 +0100

    Revert "Bug/pa ingester parent ref (#1110)"
    This reverts commit 02cd6f9756b3b207c8ad9ec33c829c46582d8111.

--- src/main/java/org/atlasapi/remotesite/pa/PaProgrammeProcessor.java
+++ src/main/java/org/atlasapi/remotesite/pa/PaProgrammeProcessor.java
@@ -203,10 +203,6 @@ public class PaProgrammeProcessor implements PaProgDataProcessor, PaProgDataUpda
                     progData, isEpisode
             );
 
-            if (!isEpisode) {
-                item.setParentRef(null);
-            }
-
             item.setGenericDescription(isGenericDescription(progData));
             item.addAlias(PaHelper.getProgIdAlias(progData.getProgId()));
             item.setLastUpdated(updatedAt.toDateTimeUTC());

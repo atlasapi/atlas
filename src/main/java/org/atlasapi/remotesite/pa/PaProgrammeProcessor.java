b983769b36867c4fa521e59fbc6325ddd6b9d246
Jake Keeys <jake@metabroadcast.com>
2016-10-28 16:18:13 +0100

    Add new Alias's for PA Films; Remove now redundant RtFilmAlias

--- src/main/java/org/atlasapi/remotesite/pa/PaProgrammeProcessor.java
+++ src/main/java/org/atlasapi/remotesite/pa/PaProgrammeProcessor.java
@@ -656,18 +656,13 @@ public class PaProgrammeProcessor implements PaProgDataProcessor, PaProgDataUpda
         // and only set the film alias when a rt_filmnumber value exists, we make that
         // check here.
 
-        if (!Strings.isNullOrEmpty(progData.getRtFilmnumber())) {
-            episode.addAlias(PaHelper.getFilmAlias(identifierFor(progData)));
-        }
-
-        episode.setAliasUrls(ImmutableSet.of(PaHelper.getAlias(progData.getProgId())));
-
         Optional<String> rtFilmIdentifier = rtFilmIdentifierFor(progData);
         if (rtFilmIdentifier.isPresent()) {
-            episode.addAlias(PaHelper.getRtFilmAlias(rtFilmIdentifier.get()));
             episode.addAliasUrl(PaHelper.getFilmRtAlias(rtFilmIdentifier.get()));
+            episode.addAlias(PaHelper.getLegacyFilmAlias(rtFilmIdentifier.get()));
         }
-
+        episode.addAliasUrl(PaHelper.getAlias(progData.getProgId()));
+        episode.addAlias(PaHelper.getNewFilmAlias(identifierFor(progData)));
     }
 
     private Broadcast addBroadcast(ProgData progData, Item item, Channel channel,

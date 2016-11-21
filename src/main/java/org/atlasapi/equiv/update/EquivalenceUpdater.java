035ceca84007e233c3e824fa44d45956882a2b4a
Thomas Maroulis <thomas@metabroadcast.com>
2016-11-21 16:21:27 +0000

    Merge conflicts

--- src/main/java/org/atlasapi/equiv/update/EquivalenceUpdater.java
+++ src/main/java/org/atlasapi/equiv/update/EquivalenceUpdater.java
@@ -1,13 +1,20 @@
 package org.atlasapi.equiv.update;
 
+import java.util.Optional;
 import java.util.Set;
 
 import org.atlasapi.equiv.update.metadata.EquivalenceUpdaterMetadata;
 import org.atlasapi.media.entity.Publisher;
 
+import com.metabroadcast.columbus.telescope.client.IngestTelescopeClientImpl;
+
 public interface EquivalenceUpdater<T> {
 
-    boolean updateEquivalences(T subject);
+    boolean updateEquivalences(
+            T subject,
+            Optional<String> taskId,
+            IngestTelescopeClientImpl telescopeClient
+    );
 
     EquivalenceUpdaterMetadata getMetadata(Set<Publisher> sources);
 }

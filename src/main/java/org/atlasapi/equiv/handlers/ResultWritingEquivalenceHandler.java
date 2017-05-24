a15863d1fc2b06175f50e9755b5761394405990a
dragons <piticadragos@yahoo.com>
2017-05-24 17:19:32 +0100

    check equiv result in JSON format

--- src/main/java/org/atlasapi/equiv/handlers/ResultWritingEquivalenceHandler.java
+++ src/main/java/org/atlasapi/equiv/handlers/ResultWritingEquivalenceHandler.java
@@ -1,8 +1,13 @@
 package org.atlasapi.equiv.handlers;
 
+import java.io.IOException;
+
 import org.atlasapi.equiv.results.EquivalenceResult;
 import org.atlasapi.equiv.results.persistence.EquivalenceResultStore;
 import org.atlasapi.media.entity.Content;
+
+import com.google.common.base.Throwables;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class ResultWritingEquivalenceHandler<T extends Content>
         implements EquivalenceResultHandler<T> {
@@ -12,9 +17,18 @@ public class ResultWritingEquivalenceHandler<T extends Content>
     public ResultWritingEquivalenceHandler(EquivalenceResultStore store) {
         this.store = store;
     }
-    
+
     @Override
     public boolean handle(EquivalenceResult<T> result) {
+
+        //CloseableHttpClient client = HttpClients.createDefault();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            System.out.println(objectMapper.writeValueAsString(result));
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+
         store.store(result);
         return false;
     }

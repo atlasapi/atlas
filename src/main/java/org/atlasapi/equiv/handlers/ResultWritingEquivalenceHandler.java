7990a01205070a16a8c978d33a937dc149879368
dragons <piticadragos@yahoo.com>
2017-05-26 10:30:16 +0100

    removed unused imports

--- src/main/java/org/atlasapi/equiv/handlers/ResultWritingEquivalenceHandler.java
+++ src/main/java/org/atlasapi/equiv/handlers/ResultWritingEquivalenceHandler.java
@@ -1,10 +1,6 @@
 package org.atlasapi.equiv.handlers;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.Base64;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -16,28 +12,19 @@ import org.atlasapi.equiv.results.scores.ScoredCandidate;
 import org.atlasapi.equiv.results.scores.ScoredCandidates;
 import org.atlasapi.media.entity.Content;
 
-import com.metabroadcast.common.persistence.translator.TranslatorUtils;
 import com.metabroadcast.common.time.DateTimeZones;
 
 import com.google.common.base.Throwables;
 import com.google.common.collect.Ordering;
-import com.mongodb.BasicDBList;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.joda.time.DateTime;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.springframework.util.SerializationUtils;
 
 import static com.google.common.collect.ImmutableSet.copyOf;
-import static com.google.common.collect.Iterables.transform;
-import static com.metabroadcast.common.persistence.mongo.MongoConstants.ID;
 import static org.atlasapi.media.entity.Identified.TO_URI;
 
 public class ResultWritingEquivalenceHandler<T extends Content>

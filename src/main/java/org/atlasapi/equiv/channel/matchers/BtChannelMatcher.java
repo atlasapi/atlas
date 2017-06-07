b4c5274a3463b96f8c9fb65ca6255dd9e8091489
Jamie Coleman <jamiec@metabroadcast.com>
2017-06-07 16:24:19 +0100

    pr feedback

--- src/main/java/org/atlasapi/equiv/channel/matchers/BtChannelMatcher.java
+++ src/main/java/org/atlasapi/equiv/channel/matchers/BtChannelMatcher.java
@@ -1,17 +1,22 @@
 package org.atlasapi.equiv.channel.matchers;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Objects;
 import com.metabroadcast.common.ids.SubstitutionTableNumberCodec;
 import org.atlasapi.media.channel.Channel;
 import org.atlasapi.media.entity.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class BtChannelMatcher implements ChannelMatcher {
+public class BtChannelMatcher extends ChannelMatcher {
 
+    private static final Logger log = LoggerFactory.getLogger(BtChannelMatcher.class);
     private static final SubstitutionTableNumberCodec codec = SubstitutionTableNumberCodec.lowerCaseOnly();
     private final String uriPrefix;
 
     private BtChannelMatcher(Publisher publisher) {
-        uriPrefix = String.format("http://%s/", publisher);
+        super(publisher);
+        this.uriPrefix = String.format("http://%s/", publisher);
     }
 
     public static BtChannelMatcher create(Publisher publisher) {
@@ -20,16 +25,21 @@ public class BtChannelMatcher implements ChannelMatcher {
 
     @Override
     public boolean isAMatch(Channel existing, Channel candidate) {
+        try {
+            verifyChannelsPublishers(existing, candidate);
+        } catch (IllegalArgumentException e) {
+            log.error("Invalid channels passed to matcher", e);
+            return false;
+        }
 
         String baseChannelId = extractBaseIdFromUri(existing);
-
         long extractedBaseChannelId = codec.decode(baseChannelId).longValue();
 
         return Objects.equal(extractedBaseChannelId, candidate.getId());
     }
 
-    private String extractBaseIdFromUri(Channel channel) {
-
+    @VisibleForTesting
+    String extractBaseIdFromUri(Channel channel) {
         return channel.getUri()
                 .replace(uriPrefix, "")
                 .trim();
--- src/test/java/org/atlasapi/equiv/channel/matchers/BtChannelMatcherTest.java
+++ src/test/java/org/atlasapi/equiv/channel/matchers/BtChannelMatcherTest.java
@@ -3,6 +3,7 @@ package org.atlasapi.equiv.channel.matchers;
 import com.metabroadcast.common.ids.SubstitutionTableNumberCodec;
 import org.atlasapi.media.channel.Channel;
 import org.atlasapi.media.entity.Publisher;
+import org.junit.Before;
 import org.junit.Test;
 
 import static java.lang.String.format;
@@ -12,13 +13,18 @@ import static org.junit.Assert.assertTrue;
 public class BtChannelMatcherTest {
 
     private SubstitutionTableNumberCodec codec = SubstitutionTableNumberCodec.lowerCaseOnly();
-    private BtChannelMatcher btChannelMatcher = BtChannelMatcher.create(Publisher.BT_TV_CHANNELS);
+    private ChannelMatcher btChannelMatcher = BtChannelMatcher.create(Publisher.BT_TV_CHANNELS);
 
-    private Channel existingChannel =  channelFor(
-            Publisher.BT_TV_CHANNELS,
-            codec.decode("bbf").longValue(),
-            "dff"
-    );
+    private Channel existingChannel;
+
+    @Before
+    public void setUp() {
+        existingChannel = channelFor(
+                Publisher.BT_TV_CHANNELS,
+                codec.decode("bbf").longValue(),
+                "dff"
+        );
+    }
 
     @Test
     public void btChannelMatcherMatchesCorrectly() throws Exception {
@@ -32,7 +38,7 @@ public class BtChannelMatcherTest {
     }
 
     @Test
-    public void btChannelMatcherDoesNotMatchOnNonMatchingPaIds() throws Exception {
+    public void btChannelMatcherDoesNotMatchOnNonMatchingAtlasId() throws Exception {
         Channel nonMatchingCandidate = channelFor(
                 Publisher.METABROADCAST,
                 codec.decode("xzz").longValue(),
@@ -40,6 +46,34 @@ public class BtChannelMatcherTest {
         );
 
         assertFalse(btChannelMatcher.isAMatch(existingChannel, nonMatchingCandidate));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void channelMatcherReturnsFalseWhenMismatchedSubjectPublishers() throws Exception {
+        Channel wrongPublisherChannel = channelFor(
+                Publisher.BT_TV_CHANNELS_TEST1,
+                123L,
+                "bbd"
+        );
+
+        Channel correctCandidate = channelFor(
+                Publisher.METABROADCAST,
+                444L,
+                "xzx"
+        );
+
+        btChannelMatcher.verifyChannelsPublishers(wrongPublisherChannel, correctCandidate);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void channelMatcherReturnsFalseWhenMismatchedCandidatePublishers() throws Exception {
+        Channel invalidCandidate = channelFor(
+                Publisher.BT,
+                123L,
+                "xzz"
+        );
+
+        btChannelMatcher.verifyChannelsPublishers(existingChannel, invalidCandidate);
     }
 
     private Channel channelFor(Publisher publisher, long thisChannelId, String baseChannelId) {

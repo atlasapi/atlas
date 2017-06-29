221b6efa73d5e7011f4da65e2a81cde059ed1011
Adam Millican <am886@kent.ac.uk>
2017-06-29 13:47:38 +0100

    hardcode force equivalence matches

--- src/main/java/org/atlasapi/equiv/generators/BarbAliasEquivalenceGenerator.java
+++ src/main/java/org/atlasapi/equiv/generators/BarbAliasEquivalenceGenerator.java
@@ -1,6 +1,8 @@
 package org.atlasapi.equiv.generators;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 import com.google.common.base.Optional;
@@ -13,6 +15,7 @@ import org.atlasapi.equiv.results.scores.ScoredCandidates;
 import org.atlasapi.media.entity.Content;
 import org.atlasapi.media.entity.Identified;
 import org.atlasapi.persistence.content.ContentResolver;
+import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.persistence.lookup.entry.LookupEntry;
 import org.atlasapi.persistence.lookup.mongo.MongoLookupEntryStore;
 
@@ -43,41 +46,99 @@ public class BarbAliasEquivalenceGenerator<T extends Content> implements Equival
         DefaultScoredCandidates.Builder<T> equivalents =
                 DefaultScoredCandidates.fromSource("Barb Alias");
 
-        desc.startStage("Resolving Barb Aliases:");
-        subject.getAliasUrls().forEach(alias -> desc.appendText(alias));
-        desc.finishStage();
+        List<String> forceTargetUrisOne = new ArrayList<>();
+        forceTargetUrisOne.add("http://txlogs.barb.co.uk/episode/00000000000226905561");
+        forceTargetUrisOne.add("http://itv.com/episode/1_5576_0002");
 
-        Iterable<LookupEntry> entries = lookupEntryStore.entriesForAliases(
-                Optional.of("aliases"),
-                subject.getAliasUrls()
+        equivalents = findForcedEquivalents(
+                subject,
+                equivalents,
+                "http://cdmf.barb.co.uk/episode/754382",
+                forceTargetUrisOne,
+                desc
         );
 
-        StreamSupport.stream(entries.spliterator(), false)
-                .forEach(
-                        entry -> {
-                            Identified identified = resolver.findByCanonicalUris(
-                                    ImmutableSet.of(entry.uri())
-                            ).getFirstValue().requireValue();
+        List<String> forceTargetUrisTwo = new ArrayList<>();
+        forceTargetUrisTwo.add("http://txlogs.barb.co.uk/episode/00000000000228464843");
+        forceTargetUrisTwo.add("http://itv.com/episode/9D_13136");
 
-                            if (identified.getAliasUrls().size() != 0) {
-                                boolean match = false;
+        equivalents = findForcedEquivalents(
+                subject,
+                equivalents,
+                "http://cdmf.barb.co.uk/episode/319451",
+                forceTargetUrisTwo,
+                desc
+        );
 
-                                for (String alias : identified.getAliasUrls()) {
-                                    if (subject.getAliasUrls().contains(alias)) {
-                                        match = true;
-                                    }
-                                }
+        List<String> forceTargetUrisThree = new ArrayList<>();
+        forceTargetUrisThree.add("http://txlogs.barb.co.uk/episode/107089899");
+        forceTargetUrisThree.add("http://nitro.bbc.co.uk/programmes/b0676dvn");
 
-                                if (match) {
-                                    equivalents.addEquivalent((T) identified, Score.ONE);
-                                    desc.appendText("Resolved %s", identified.getCanonicalUri());
-                                }
-                            }
-                        }
-                );
+        equivalents = findForcedEquivalents(
+                subject,
+                equivalents,
+                "http://cdmf.barb.co.uk/episode/150272",
+                forceTargetUrisThree,
+                desc
+        );
 
+        List<String> forceTargetUrisFour = new ArrayList<>();
+        forceTargetUrisFour.add("http://txlogs.barb.co.uk/episode/107089910");
+        forceTargetUrisFour.add("http://nitro.bbc.co.uk/programmes/b08vd13d");
+
+        equivalents = findForcedEquivalents(
+                subject,
+                equivalents,
+                "http://cdmf.barb.co.uk/episode/825344",
+                forceTargetUrisFour,
+                desc
+        );
+
+        List<String> forceTargetUrisFive = new ArrayList<>();
+        forceTargetUrisFive.add("http://uktv.co.uk/ENIE812H");
+        forceTargetUrisFive.add("http://txlogs.barb.co.uk/episode/ENIE812H82");
+
+        equivalents = findForcedEquivalents(
+                subject,
+                equivalents,
+                "http://cdmf.barb.co.uk/episode/754384",
+                forceTargetUrisFive,
+                desc
+        );
+
+        List<String> forceTargetUrisSix = new ArrayList<>();
+        forceTargetUrisSix.add("http://txlogs.barb.co.uk/episode/ENIE813B82");
+        forceTargetUrisSix.add("http://uktv.co.uk/ENIE813B");
+
+        equivalents = findForcedEquivalents(
+                subject,
+                equivalents,
+                "http://cdmf.barb.co.uk/episode/754382",
+                forceTargetUrisSix,
+                desc
+        );
 
         return equivalents.build();
+    }
+
+    private DefaultScoredCandidates.Builder findForcedEquivalents(
+            T subject,
+            DefaultScoredCandidates.Builder equivalents,
+            String forceSubjectUri,
+            List<String> forceTargetUris,
+            ResultDescription desc
+    ) {
+        if (subject.getCanonicalUri().equals(forceSubjectUri)) {
+
+            ResolvedContent resolvedContent = resolver.findByCanonicalUris(forceTargetUris);
+
+            resolvedContent.getAllResolvedResults().forEach(identified -> {
+                equivalents.addEquivalent((T) identified, Score.ONE);
+                desc.appendText("Resolved %s", identified.getCanonicalUri());
+            });
+
+        }
+        return equivalents;
     }
 
     @Override

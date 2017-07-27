b343d9c1591dd489b8dc06204225d6689e4a93da
Jamie Coleman <jamiec@metabroadcast.com>
2017-07-27 16:15:24 +0100

    remove configs, remove code

--- src/main/java/org/atlasapi/remotesite/btvod/BtVodContentMatchingPredicate.java
+++
@@ -1,9 +0,0 @@
-package org.atlasapi.remotesite.btvod;
-
-import com.google.common.base.Predicate;
-
-
-public interface BtVodContentMatchingPredicate extends Predicate<VodEntryAndContent> {
-
-    void init();
-}
--- src/main/java/org/atlasapi/remotesite/btvod/package-info.java
+++
@@ -1,5 +0,0 @@
-@NonNullByDefault
-package org.atlasapi.remotesite.btvod;
-
-import com.metabroadcast.common.annotation.NonNullByDefault;
-

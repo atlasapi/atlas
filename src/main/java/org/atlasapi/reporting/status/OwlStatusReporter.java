03fe683e09a263a7cb02c03a6f7c63ca4d4b1b5d
sandra <cotton_glow@hotmail.com>
2017-12-14 15:38:11 +0000

    updated log reporting

--- src/main/java/org/atlasapi/reporting/status/OwlStatusReporter.java
+++ src/main/java/org/atlasapi/reporting/status/OwlStatusReporter.java
@@ -51,15 +51,13 @@ public class OwlStatusReporter {
     }
 
     public void updateStatus(EntityRef.Type type, Long id, PartialStatus partialStatus) {
-        log.info("update status start");
-        log.error("update status start");
         if (statusClientWithApp != null){
             try {
                 log.error("Status is about to be updated.");
                 statusClientWithApp.updateStatus(appId, type, id, partialStatus);
                 log.info("Status updates for " + id + ".");
             } catch (Exception e) {
-                log.error("An unknown exception occured during .updateStatus " + e.getMessage() + ".\n"+
+                log.error("An unknown exception occured during .updateStatus " + e + ".\n"+
                         "StatusReporter has protected you from this problem.");
             }
         } else {

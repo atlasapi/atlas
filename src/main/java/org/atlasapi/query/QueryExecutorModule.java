cecd1a9ab25efdb32023a282e048fdb18d0421c8
@alianos- <alianos.home@gmail.com>
2018-02-19 18:29:17 +0000

    bug fix

--- src/main/java/org/atlasapi/query/QueryExecutorModule.java
+++ src/main/java/org/atlasapi/query/QueryExecutorModule.java
@@ -116,7 +116,7 @@ public class QueryExecutorModule {
                 new DefaultJacksonModelReader(),
                 delegatingModelTransformer(),
                 contentResolver,
-                contentWrier,
+                contentWriter,
                 scheduleWriter,
                 channelResolver,
                 eventResolver,

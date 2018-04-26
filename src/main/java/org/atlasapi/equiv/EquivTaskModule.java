add0546c20b1fe35914df345b5153b460eb45fbe
@alianos- <alianos.home@gmail.com>
2018-04-26 17:35:38 +0100

    changed amazon equiv schedule to 8:00am from 3:00am (allow more time to index content)

--- src/main/java/org/atlasapi/equiv/EquivTaskModule.java
+++ src/main/java/org/atlasapi/equiv/EquivTaskModule.java
@@ -154,7 +154,7 @@ public class EquivTaskModule {
     private static final RepetitionRule BT_VOD_EQUIVALENCE_REPETITION =
             RepetitionRules.daily(new LocalTime(3, 0));
     private static final RepetitionRule AMAZON_EQUIVALENCE_REPETITION =
-            RepetitionRules.daily(new LocalTime(3, 0));
+            RepetitionRules.daily(new LocalTime(8, 0));
     private static final RepetitionRule UKTV_EQUIVALENCE_REPETITION =
             RepetitionRules.daily(new LocalTime(20, 0));
     private static final RepetitionRule WIKIPEDIA_EQUIVALENCE_REPETITION =

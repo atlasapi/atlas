e2281b5d0b4af353e3687abbbde9d27a252f2c1e
@alianos- <alianos.home@gmail.com>
2018-06-18 17:43:06 +0100

    try to load in memory

--- src/main/java/org/atlasapi/remotesite/amazonunbox/AmazonUnboxHttpFeedSupplier.java
+++ src/main/java/org/atlasapi/remotesite/amazonunbox/AmazonUnboxHttpFeedSupplier.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collections;
@@ -14,14 +15,19 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.atlasapi.remotesite.pa.channels.PaChannelGroupsIngester;
+
 import com.metabroadcast.common.http.HttpStatusCode;
 
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableList;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
@@ -39,7 +45,15 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class AmazonUnboxHttpFeedSupplier implements Supplier<ImmutableList<AmazonUnboxItem>> {
 
+    private static final Logger log = LoggerFactory.getLogger(AmazonUnboxHttpFeedSupplier.class);
+
     private final String uri;
+    String xml10pattern = "[^"
+                          + "\u0009\r\n"
+                          + "\u0020-\uD7FF"
+                          + "\uE000-\uFFFD"
+                          + "\ud800\udc00-\udbff\udfff"
+                          + "]";
 
     public AmazonUnboxHttpFeedSupplier(String uri) {
         this.uri = checkNotNull(uri);
@@ -66,18 +80,37 @@ public class AmazonUnboxHttpFeedSupplier implements Supplier<ImmutableList<Amazo
             ZipInputStream zis = new ZipInputStream(response.getEntity().getContent());
             zis.getNextEntry();
 
-            String beginning = "<?xml version=\"1.1\" ?>";
-            List<InputStream> streams = Arrays.asList(
-                    new ByteArrayInputStream(beginning.getBytes()), zis);
-            InputStream is = new SequenceInputStream(Collections.enumeration(streams));
+            log.info("Attempting to load the whole amazon catalogue in memory.");
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(zis, writer, "UTF-8");
+            String theString = writer.toString();
+            log.info("Succeeded loading amazon catalogue in memory. Attempting to replace illegal characters.");
+            String theCleanedString = theString.replaceAll(xml10pattern, "");
+            log.info("Succeeded replacing illegal characters.");
 
-            saxParser.parse(is, handler);
+            //  FilteringInputStream fis = new FilteringInputStream(zis);
+
+            saxParser.parse(theCleanedString, handler);
             zis.close();
 
             return processor.getResult();
 
         } catch (IOException | ParserConfigurationException | SAXException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private class FilteringInputStream extends InputStream{
+
+        InputStream delegate;
+
+        public FilteringInputStream(InputStream delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public int read() throws IOException {
+            return delegate.read();
         }
     }
 

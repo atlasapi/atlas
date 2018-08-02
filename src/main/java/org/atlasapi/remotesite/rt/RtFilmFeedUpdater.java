c0f0dc399b59ecff0afaf4161f7c584982d04f8d
Adam Millican <am886@kent.ac.uk>
2018-08-02 14:54:28 +0100

    changed how rt film feed gets and transforms via http

--- src/main/java/org/atlasapi/remotesite/rt/RtFilmFeedUpdater.java
+++ src/main/java/org/atlasapi/remotesite/rt/RtFilmFeedUpdater.java
@@ -3,20 +3,20 @@ package org.atlasapi.remotesite.rt;
 import static org.atlasapi.persistence.logging.AdapterLogEntry.errorEntry;
 
 import java.io.InputStreamReader;
-import java.util.concurrent.TimeUnit;
 
 import nu.xom.Builder;
 import nu.xom.Element;
 import nu.xom.NodeFactory;
 import nu.xom.Nodes;
 
-import org.atlasapi.http.AbstractHttpResponseTransformer;
+import org.apache.http.client.fluent.Request;
+
+import org.apache.http.client.fluent.Response;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.logging.AdapterLog;
 import org.atlasapi.persistence.logging.AdapterLogEntry;
 import org.atlasapi.persistence.logging.AdapterLogEntry.Severity;
-import org.atlasapi.remotesite.HttpClients;
 import org.atlasapi.reporting.telescope.OwlTelescopeReporter;
 import org.atlasapi.reporting.telescope.OwlTelescopeReporterFactory;
 import org.atlasapi.reporting.telescope.OwlTelescopeReporters;
@@ -31,10 +31,6 @@ import com.google.common.base.Throwables;
 
 import com.metabroadcast.columbus.telescope.api.Event;
 import com.metabroadcast.columbus.telescope.client.EntityType;
-import com.metabroadcast.common.http.HttpResponseTransformer;
-import com.metabroadcast.common.http.SimpleHttpClient;
-import com.metabroadcast.common.http.SimpleHttpClientBuilder;
-import com.metabroadcast.common.http.SimpleHttpRequest;
 import com.metabroadcast.common.scheduling.ScheduledTask;
 import com.metabroadcast.common.url.UrlEncoding;
 
@@ -42,48 +38,44 @@ public class RtFilmFeedUpdater extends ScheduledTask {
     
     private final static DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss");
     private final static DateTime START_DATE = new DateTime(2011, DateTimeConstants.APRIL, 12, 0, 0, 0, 0);
-    
-    private final SimpleHttpClient client = new SimpleHttpClientBuilder()
-        .withUserAgent(HttpClients.ATLAS_USER_AGENT)
-        .withSocketTimeout(5, TimeUnit.MINUTES)
-        .withConnectionTimeout(1, TimeUnit.MINUTES)
-    .build();
-    
-    private final HttpResponseTransformer<Void> TRANSFORMER = new AbstractHttpResponseTransformer<Void>() {
 
-        @Override
-        protected Void transform(InputStreamReader bodyReader) throws Exception {
-            try {
-                FilmProcessingNodeFactory filmProcessingNodeFactory = new FilmProcessingNodeFactory();
-                Builder builder = new Builder(filmProcessingNodeFactory);
-                builder.build(bodyReader);
-                reportStatus(String.format("Finished. Processed %s. %s failed", filmProcessingNodeFactory.getProcessed(), filmProcessingNodeFactory.getFailed()));
-            } catch (Exception e) {
-                log.record(AdapterLogEntry.errorEntry().withCause(e).withSource(getClass()).withDescription("Exception in RT Film updater"));
-            }
-            return null;
-        }
-        
-    };
-    
     private final String feedUrl;
     private final AdapterLog log;
     private final RtFilmProcessor processor;
     private final boolean doCompleteUpdate;
     private OwlTelescopeReporter telescopeReporter;
 
-    public RtFilmFeedUpdater(String feedUrl, AdapterLog log, ContentResolver contentResolver, ContentWriter contentWriter, RtFilmProcessor processor) {
+    public RtFilmFeedUpdater(
+            String feedUrl,
+            AdapterLog log,
+            ContentResolver contentResolver,
+            ContentWriter contentWriter,
+            RtFilmProcessor processor
+    ) {
         this(feedUrl, log, contentResolver, contentWriter, processor, false);
     }
     
-    private RtFilmFeedUpdater(String feedUrl, AdapterLog log, ContentResolver contentResolver, ContentWriter contentWriter, RtFilmProcessor processor, boolean doCompleteUpdate) {
+    private RtFilmFeedUpdater(
+            String feedUrl,
+            AdapterLog log,
+            ContentResolver contentResolver,
+            ContentWriter contentWriter,
+            RtFilmProcessor processor,
+            boolean doCompleteUpdate
+    ) {
         this.feedUrl = feedUrl;
         this.log = log;
         this.processor = processor;
         this.doCompleteUpdate = doCompleteUpdate;
     }
     
-    public static RtFilmFeedUpdater completeUpdater(String feedUrl, AdapterLog log, ContentResolver contentResolver, ContentWriter contentWriter, RtFilmProcessor processor) {
+    public static RtFilmFeedUpdater completeUpdater(
+            String feedUrl,
+            AdapterLog log,
+            ContentResolver contentResolver,
+            ContentWriter contentWriter,
+            RtFilmProcessor processor
+    ) {
         return new RtFilmFeedUpdater(feedUrl, log, contentResolver, contentWriter, processor, true);
     }
     
@@ -100,14 +92,19 @@ public class RtFilmFeedUpdater extends ScheduledTask {
         if (doCompleteUpdate) {
             requestUri += "/since?lastUpdated=" + UrlEncoding.encode(dateFormat.print(START_DATE));
         } else {
-            requestUri += "/since?lastUpdated=" + UrlEncoding.encode(dateFormat.print(new DateTime(DateTimeZone.UTC).minusDays(3)));
+            requestUri += "/since?lastUpdated=" + UrlEncoding
+                    .encode(dateFormat.print(new DateTime(DateTimeZone.UTC).minusDays(3)));
         }
         
         try {
             reportStatus("Started...");
-            client.get(new SimpleHttpRequest<Void>(requestUri, TRANSFORMER));
+            getAndTransform(feedUrl);
         } catch (Exception e) {
-            AdapterLogEntry errorRecord = errorEntry().withCause(e).withSource(getClass()).withUri(requestUri).withDescription("Exception while fetching film feed");
+            AdapterLogEntry errorRecord = errorEntry()
+                    .withCause(e)
+                    .withSource(getClass())
+                    .withUri(requestUri)
+                    .withDescription("Exception while fetching film feed");
             log.record(errorRecord);
             reportStatus("Failed: " + errorRecord.id());
             telescopeReporter.reportFailedEvent("Failed with exception. Ingest stopped abruptly. " + e.getMessage(),
@@ -116,6 +113,24 @@ public class RtFilmFeedUpdater extends ScheduledTask {
         } finally {
             telescopeReporter.endReporting();
         }
+    }
+
+    private void getAndTransform(String feedUrl) throws Exception {
+        Response response = Request.Get("http://somehost/")
+                .connectTimeout(1000)
+                .socketTimeout(1000)
+                .execute();
+
+        InputStreamReader responseStream = new InputStreamReader(response.returnContent().asStream());
+
+        FilmProcessingNodeFactory filmProcessingNodeFactory = new FilmProcessingNodeFactory();
+        Builder builder = new Builder(filmProcessingNodeFactory);
+        builder.build(responseStream);
+        reportStatus(String.format(
+                "Finished. Processed %s. %s failed",
+                filmProcessingNodeFactory.getProcessed(),
+                filmProcessingNodeFactory.getFailed()
+        ));
     }
 
     private class FilmProcessingNodeFactory extends NodeFactory {
@@ -130,7 +145,10 @@ public class RtFilmFeedUpdater extends ScheduledTask {
                     processor.process(element, telescopeReporter);
                 }
                 catch (Exception e) {
-                    log.record(new AdapterLogEntry(Severity.ERROR).withSource(RtFilmFeedUpdater.class).withCause(e).withDescription("Exception when processing film"));
+                    log.record(new AdapterLogEntry(Severity.ERROR)
+                            .withSource(RtFilmFeedUpdater.class)
+                            .withCause(e)
+                            .withDescription("Exception when processing film"));
                     failures++;
                     telescopeReporter.reportFailedEvent("Failed with exception. " + e.getMessage(),
                             EntityType.FILM, element);

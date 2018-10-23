81ae92eadfb25f939d535313e835d251e5a5b2b6
jamiesprax <jamiesprax@gmail.com>
2018-10-23 16:54:09 +0100

    fix test

--- src/test/java/org/atlasapi/equiv/generators/ExactTitleGeneratorTest.java
+++ src/test/java/org/atlasapi/equiv/generators/ExactTitleGeneratorTest.java
@@ -1,19 +1,17 @@
 package org.atlasapi.equiv.generators;
 
-import com.google.common.collect.ImmutableList;
-import com.metabroadcast.applications.client.model.internal.Application;
+import com.google.common.collect.ImmutableMap;
 import org.atlasapi.equiv.results.description.DefaultDescription;
 import org.atlasapi.equiv.results.scores.ScoredCandidates;
 import org.atlasapi.equiv.update.metadata.EquivToTelescopeResults;
-import org.atlasapi.media.entity.Identified;
+import org.atlasapi.media.entity.Content;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Publisher;
-import org.atlasapi.persistence.content.SearchResolver;
-import org.atlasapi.search.model.SearchQuery;
+import org.atlasapi.persistence.content.ResolvedContent;
+import org.atlasapi.persistence.content.mongo.MongoContentResolver;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
@@ -27,7 +25,7 @@ import static org.mockito.Mockito.when;
 public class ExactTitleGeneratorTest {
 
     private ExactTitleGenerator<Item> generator;
-    private SearchResolver resolver = mock(SearchResolver.class);
+    private MongoContentResolver resolver = mock(MongoContentResolver.class);
 
     @Before
     public void setUp() {
@@ -45,11 +43,17 @@ public class ExactTitleGeneratorTest {
         Item item2 = makeItem(2L, "ite", Publisher.AMAZON_UNBOX);
         Item item3 = makeItem(3L, "ite", Publisher.AMAZON_UNBOX);
 
-        List<Identified> retList = ImmutableList.of(item1, item2, item3);
+        ResolvedContent retList = ResolvedContent.builder().putAll(
+                ImmutableMap.of(
+                        item1.getCanonicalUri(), item1,
+                        item2.getCanonicalUri(), item2,
+                        item3.getCanonicalUri(), item3
+                )
+        ).build();
 
         Item subject = makeItem(2L, "ite", Publisher.AMAZON_UNBOX);
 
-        when(resolver.search(any(SearchQuery.class), any(Application.class))).thenReturn(retList);
+        when(resolver.getExactTitleMatches(any(Content.class))).thenReturn(retList);
 
         ScoredCandidates<Item> candidates = generator.generate(
                 subject,
@@ -71,11 +75,18 @@ public class ExactTitleGeneratorTest {
         Item item3 = makeItem(3L, "iTe", Publisher.AMAZON_UNBOX);
         Item item4 = makeItem(4L, "ITE", Publisher.AMAZON_UNBOX);
 
-        List<Identified> retList = ImmutableList.of(item1, item2, item3);
+        ResolvedContent retList = ResolvedContent.builder().putAll(
+                ImmutableMap.of(
+                        item1.getCanonicalUri(), item1,
+                        item2.getCanonicalUri(), item2,
+                        item3.getCanonicalUri(), item3,
+                        item4.getCanonicalUri(), item4
+                )
+        ).build();
 
         Item subject = makeItem(2L, "ite", Publisher.AMAZON_UNBOX);
 
-        when(resolver.search(any(SearchQuery.class), any(Application.class))).thenReturn(retList);
+        when(resolver.getExactTitleMatches(any(Content.class))).thenReturn(retList);
 
         ScoredCandidates<Item> candidates = generator.generate(
                 subject,
@@ -97,11 +108,16 @@ public class ExactTitleGeneratorTest {
         Item item2 = makeItem(2L, "日の出の海と瞑想音楽", Publisher.AMAZON_UNBOX);
         Item item3 = makeItem(3L, "日の", Publisher.AMAZON_UNBOX);
 
-        List<Identified> retList = ImmutableList.of(item1, item2);
+        ResolvedContent retList = ResolvedContent.builder().putAll(
+                ImmutableMap.of(
+                        item1.getCanonicalUri(), item1,
+                        item2.getCanonicalUri(), item2
+                )
+        ).build();
 
         Item subject = makeItem(2L, "日の出の海と瞑想音楽", Publisher.AMAZON_UNBOX);
 
-        when(resolver.search(any(SearchQuery.class), any(Application.class))).thenReturn(retList);
+        when(resolver.getExactTitleMatches(any(Content.class))).thenReturn(retList);
 
         ScoredCandidates<Item> candidates = generator.generate(
                 subject,
@@ -127,7 +143,7 @@ public class ExactTitleGeneratorTest {
         );
 
         assertThat(candidates.candidates().isEmpty(), is(true));
-        verify(resolver, never()).search(any(SearchQuery.class), (any(Application.class)));
+        verify(resolver, never()).getExactTitleMatches(any(Content.class));
     }
 
     @Test
@@ -141,11 +157,12 @@ public class ExactTitleGeneratorTest {
         );
 
         assertThat(candidates.candidates().isEmpty(), is(true));
-        verify(resolver, never()).search(any(SearchQuery.class), (any(Application.class)));
+        verify(resolver, never()).getExactTitleMatches(any(Content.class));
     }
 
     private Item makeItem(long id, String title, Publisher publisher) {
         Item item = new Item();
+        item.setCanonicalUri(title+id);
         item.setId(id);
         item.setTitle(title);
         item.setPublisher(publisher);

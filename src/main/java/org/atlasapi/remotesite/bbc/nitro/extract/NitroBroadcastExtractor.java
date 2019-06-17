e00929c72c673349c4e9dbea65d1978e0bcaff1b
Tristan Beardmore-Gray <tristan.beardmore-gray@hotmail.co.uk>
2019-06-17 16:58:22 +0100

    Added a null check for txTime field in NitroBroadcastExtractor

--- src/main/java/org/atlasapi/remotesite/bbc/nitro/extract/NitroBroadcastExtractor.java
+++ src/main/java/org/atlasapi/remotesite/bbc/nitro/extract/NitroBroadcastExtractor.java
@@ -48,16 +48,18 @@ public class NitroBroadcastExtractor
         DateTime start = NitroUtil.toDateTime(source.getPublishedTime().getStart());
         DateTime end = NitroUtil.toDateTime(source.getPublishedTime().getEnd());
 
-        DateTime actualStart = NitroUtil.toDateTime(source.getTxTime().getStart());
-        DateTime actualEnd = NitroUtil.toDateTime(source.getTxTime().getEnd());
-
         Broadcast broadcast = new Broadcast(channel, start, end)
                 .withId("bbc:" + source.getPid());
         broadcast.setRepeat(source.isIsRepeat());
         broadcast.setAudioDescribed(source.isIsAudioDescribed());
         broadcast.setAliases(extractAliasesFrom(source));
-        broadcast.setActualTransmissionTime(actualStart);
-        broadcast.setActualTransmissionEndTime(actualEnd);
+
+        if(source.getTxTime() != null) {
+            DateTime actualStart = NitroUtil.toDateTime(source.getTxTime().getStart());
+            DateTime actualEnd = NitroUtil.toDateTime(source.getTxTime().getEnd());
+            broadcast.setActualTransmissionTime(actualStart);
+            broadcast.setActualTransmissionEndTime(actualEnd);
+        }
 
         // Adding an alias uri for equivalence between Nitro and YV broadcasts
         Optional<Alias> pcridAlias = Iterables.tryFind(broadcast.getAliases(), PCRID_ALIAS);

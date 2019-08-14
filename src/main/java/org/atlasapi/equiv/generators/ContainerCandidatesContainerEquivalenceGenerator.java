a9900151ad50e9f9d15071a9c13f5b494a523374
EmiEnache <emi@metabroadcast.com>
2019-08-14 11:18:20 +0100

    Add logs to ContainerCandidatesContainer generator

--- src/main/java/org/atlasapi/equiv/generators/ContainerCandidatesContainerEquivalenceGenerator.java
+++ src/main/java/org/atlasapi/equiv/generators/ContainerCandidatesContainerEquivalenceGenerator.java
@@ -1,10 +1,12 @@
 package org.atlasapi.equiv.generators;
 
-import com.google.common.base.Function;
-import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
-import com.metabroadcast.common.collect.OptionalMap;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.annotation.Nullable;
+
 import org.atlasapi.equiv.ContentRef;
 import org.atlasapi.equiv.EquivalenceSummary;
 import org.atlasapi.equiv.EquivalenceSummaryStore;
@@ -25,10 +27,12 @@ import org.atlasapi.media.entity.SeriesRef;
 import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ResolvedContent;
 
-import javax.annotation.Nullable;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import com.metabroadcast.common.collect.OptionalMap;
+
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 
 /**
  * Generates equivalences for an non-top-level Container based on the children of the equivalences
@@ -40,15 +44,6 @@ public class ContainerCandidatesContainerEquivalenceGenerator
     private final ContentResolver contentResolver;
     private final EquivalenceSummaryStore equivSummaryStore;
     private final Set<Publisher> publishers;
-
-    private final Function<Brand, Iterable<Series>> TO_SERIES = new Function<Brand, Iterable<Series>>() {
-        @Override
-        public Iterable<Series> apply(@Nullable Brand input) {
-            Iterable<String> seriesUris = Iterables.transform(input.getSeriesRefs(), SeriesRef.TO_URI);
-            ResolvedContent series = contentResolver.findByCanonicalUris(seriesUris);
-            return Iterables.filter(series.getAllResolvedResults(), Series.class);
-        }
-    };
 
     public ContainerCandidatesContainerEquivalenceGenerator(
             ContentResolver contentResolver,
@@ -76,48 +71,60 @@ public class ContainerCandidatesContainerEquivalenceGenerator
         Builder<Container> result = DefaultScoredCandidates.fromSource("Container");
         EquivToTelescopeComponent generatorComponent = EquivToTelescopeComponent.create();
         generatorComponent.setComponentName("Container Candidates Container Equivalence Generator");
-        if (subject instanceof Series) {
-            Series series = (Series) subject;
-            ParentRef parent = series.getParent();
-            if (parent != null) {
-                String parentUri = parent.getUri();
-                OptionalMap<String, EquivalenceSummary> containerSummary = topLevelSummary(parentUri);
-                Optional<EquivalenceSummary> optional = containerSummary.get(parentUri);
-                if (optional.isPresent()) {
-                    EquivalenceSummary summary = optional.get();
-                    for (Series candidateSeries : seriesOf(
-                            Iterables.transform(summary.getEquivalents().values(), TO_CANONICAL_URI)
-                    )) {
-                        if (publishers != null){
-                            if (!publishers.contains(candidateSeries.getPublisher())){
-                                continue;
-                            }
-                        }
-                        //if its published, and its not the subject itself, we have a winner.
-                        if (candidateSeries.isActivelyPublished()
+
+        if (!(subject instanceof Series)) {
+            equivToTelescopeResult.addGeneratorResult(generatorComponent);
+            return result.build();
+        }
+
+        Series series = (Series) subject;
+        if(series.getParent() == null){
+            equivToTelescopeResult.addGeneratorResult(generatorComponent);
+            return result.build();
+        }
+
+        ParentRef parent = series.getParent();
+        String parentUri = parent.getUri();
+        OptionalMap<String, EquivalenceSummary> containerSummary = topLevelSummary(parentUri);
+        Optional<EquivalenceSummary> optional = containerSummary.get(parentUri);
+        if (optional.isPresent()) {
+            EquivalenceSummary summary = optional.get();
+            for (String containerUri : summary.getCandidates()) {
+                List<Identified> resolvedContent = contentResolver.findByCanonicalUris(
+                        Collections.singleton(containerUri)).getAllResolvedResults();
+                Brand resolvedContainer = Iterables.filter(resolvedContent, Brand.class)
+                        .iterator()
+                        .next();
+                if (publishers != null) {
+                    if (!publishers.contains(resolvedContainer.getPublisher())) {
+                        continue;
+                    }
+                }
+                for (Series candidateSeries : seriesOf(resolvedContainer)) {
+                    //if its published, and its not the subject itself, we have a winner.
+                    if (candidateSeries.isActivelyPublished()
                             && !Objects.equals(candidateSeries.getId(), subject.getId())) {
 
-                            result.addEquivalent(candidateSeries, Score.NULL_SCORE);
-
-                            generatorComponent.addComponentResult(candidateSeries.getId(), "");
-                        }
+                        result.addEquivalent(candidateSeries, Score.NULL_SCORE);
+                        desc.appendText(
+                                "Candidate: %s (from parent: %s)",
+                                candidateSeries.getCanonicalUri(),
+                                resolvedContainer.getCanonicalUri()
+                        );
+                        generatorComponent.addComponentResult(candidateSeries.getId(), "");
                     }
                 }
             }
         }
-
         equivToTelescopeResult.addGeneratorResult(generatorComponent);
 
         return result.build();
     }
 
-    private Iterable<Series> seriesOf(Iterable<String> equivalents) {
-        
-        List<Identified> resolvedContent = contentResolver
-                .findByCanonicalUris(equivalents)
-                .getAllResolvedResults();
-        Iterable<Brand> resolvedContainers = Iterables.filter(resolvedContent, Brand.class);
-        return Iterables.concat(Iterables.transform(resolvedContainers, TO_SERIES));
+    private Iterable<Series> seriesOf(Brand container){
+        Iterable<String> seriesUris = Iterables.transform(container.getSeriesRefs(), SeriesRef.TO_URI);
+        ResolvedContent series = contentResolver.findByCanonicalUris(seriesUris);
+        return Iterables.filter(series.getAllResolvedResults(), Series.class);
     }
 
     private OptionalMap<String, EquivalenceSummary> topLevelSummary(String parentUri) {

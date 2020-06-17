ef69f7f0bbc78a74fe3bd19364ca83b84fc2bea6
Oliver Broomhall <obroomhall@gmail.com>
2020-06-17 16:49:35 +0100

    Add more autowiring constructs for the hell of it.

--- src/main/java/org/atlasapi/ThematicLabels.java
+++ src/main/java/org/atlasapi/ThematicLabels.java
@@ -16,9 +16,9 @@ import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class ThematicLabels {
 
     private static final Logger log = LoggerFactory.getLogger(ThematicLabels.class);

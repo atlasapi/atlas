2c81bf0c659cb7b069ad34610c6bd1140d7c23a6
Oliver Broomhall <obroomhall@gmail.com>
2020-06-17 16:20:34 +0100

    Add @Configuration to ThematicLabels for autowiring.

--- src/main/java/org/atlasapi/ThematicLabels.java
+++ src/main/java/org/atlasapi/ThematicLabels.java
@@ -14,7 +14,9 @@ import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ThematicLabels {
 
     private static final Logger log = LoggerFactory.getLogger(ThematicLabels.class);
